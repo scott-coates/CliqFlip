c469f12fb5d451209b003c7dbaea65bab40e9d57
scottc <devnull@localhost>
2011-12-21 14:02:48 -0800

    moved

--- Solutions/CliqFlip.Web.Mvc/Content/js/home/home.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/home/home.js
@@ -1,18 +1,3 @@
-jQuery(function ($) {
-	var $ = jQuery;
-
-	$(function () {
-		var data = {
-			items: [
-				{ value: "21", name: "Sports" },
-				{ value: "43", name: "Entertainment" },
-				{ value: "46", name: "Auto" },
-				{ value: "54", name: "Shopping" },
-				{ value: "55", name: "Books" },
-				{ value: "79", name: "Aviation" }
-			]
-		};
-		
-		$("#interestSearch").autoSuggest(data.items, { selectedItemProp: "name", searchObjProps: "name", startText: "Type in some things you like" });
-	});
-});
+function InitAutoSuggest(data) {
+	$("#interestSearch").autoSuggest(data.items, { selectedItemProp: "name", searchObjProps: "name", startText: "Type in some things you like" });
+}
