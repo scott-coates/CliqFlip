be99282d72191aef84214e8eb2018e80a6ef5666
scottc <devnull@localhost>
2011-12-21 13:29:49 -0800

    added auto search

---
+++ Solutions/CliqFlip.Web.Mvc/Content/js/home/home.js
@@ -0,0 +1,17 @@
+jQuery(function($) {
+	var $ = jQuery;
+
+	$(function() {
+		var data = {
+			items: [
+				{ value: "21", name: "Mick Jagger" },
+				{ value: "43", name: "Johnny Storm" },
+				{ value: "46", name: "Richard Hatch" },
+				{ value: "54", name: "Kelly Slater" },
+				{ value: "55", name: "Rudy Hamilton" },
+				{ value: "79", name: "Michael Jordan" }
+			]
+		};
+		$("#interestSearch").autoSuggest(data.items, { selectedItemProp: "name", searchObjProps: "name" });
+	});
+});
