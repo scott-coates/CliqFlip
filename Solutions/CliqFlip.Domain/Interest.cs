0510d34a06aeaf126381d0db4158b50c53324441
scottc <devnull@localhost>
2011-12-21 12:54:47 -0800

    added

---
+++ Solutions/CliqFlip.Domain/Interest.cs
@@ -0,0 +1,11 @@
+﻿namespace CliqFlip.Domain
+{
+	using SharpArch.Domain.DomainModel;
+
+	public class Interest : Entity
+	{
+		public virtual Interest ParentInterest { get; set; }
+
+		public virtual string Name { get; set; }
+	}
+}
