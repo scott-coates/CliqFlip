0764712a8e9f4917fbf6064dfc74e00c0993338d
scottc <devnull@localhost>
2011-12-22 13:43:08 -0800

    uupdated ref

--- Solutions/CliqFlip.Domain/Entities/User.cs
+++ Solutions/CliqFlip.Domain/Entities/User.cs
@@ -1,8 +1,15 @@
-﻿using SharpArch.Domain.DomainModel;
+﻿using System.Collections.Generic;
+using SharpArch.Domain.DomainModel;
 
 namespace CliqFlip.Domain.Entities
 {
 	public class User : Entity
     {
+		public virtual ISet<Interest> Interests { get; set; }
+
+		public User()
+		{
+			 Interests = new HashSet<Interest>();
+		}
     }
 }
