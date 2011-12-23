e151880e2290cfebb8065c13288cb1f6969cb45b
scottc <devnull@localhost>
2011-12-22 17:12:49 -0800

    updated rel

---
+++ Solutions/CliqFlip.Infrastructure/NHibernateMaps/Overrides/UserOverride.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CliqFlip.Domain.Entities;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+
+namespace CliqFlip.Infrastructure.NHibernateMaps.Overrides
+{
+	public class UserOverride : IAutoMappingOverride<User>
+	{
+		public void Override(AutoMapping<User> mapping)
+		{
+			mapping.HasManyToMany(x => x.Interests);
+		}
+	}
+}
