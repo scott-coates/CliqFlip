4b19d15daa4e7d2f32c074fa77d8f767d73bac26
scottc <devnull@localhost>
2011-12-31 15:15:01 -0800

    changing it up a bit

--- Solutions/CliqFlip.Web.Mvc/Content/js/home/home.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/home/home.js
@@ -5,6 +5,9 @@ function InitAutoSuggest(data) {
 			selectedValuesProp: "Id",
 			selectedItemProp: "Name",
 			searchObjProps: "Name",
-			startText: "Type in some things you like"
+			startText: "Type in some things you like",
+			neverSubmit: true,
+			selectionAdded: function (elem) { elem.fadeTo("slow", 0.33); }
+
 		});
 }
