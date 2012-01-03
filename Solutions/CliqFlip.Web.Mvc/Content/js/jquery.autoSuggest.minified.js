66d3f469d46e808a78a25b39487be755fd7df244
scottc <devnull@localhost>
2012-01-02 23:47:36 -0800

    4making comma behave the smae

--- Solutions/CliqFlip.Web.Mvc/Content/js/jquery.autoSuggest.minified.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/jquery.autoSuggest.minified.js
@@ -189,19 +189,19 @@
 								timeout = setTimeout(function () { keyChange(); }, opts.keyDelay);
 							}
 							break;
-						case 9: case 188:  // tab or comma
-							tab_press = true;
-							var i_input = input.val().replace(/(,)/g, "");
-							if (i_input != "" && values_input.val().search("," + i_input + ",") < 0 && i_input.length >= opts.minChars) {
-								e.preventDefault();
-								var n_data = {};
-								n_data[opts.selectedItemProp] = i_input;
-								n_data[opts.selectedValuesProp] = i_input;
-								var lis = $("li", selections_holder).length;
-								add_selected_item(n_data, "00" + (lis + 1));
-								input.val("");
-							}
-						case 13: // return
+//						case 9: case 188:  // tab or comma
+//							tab_press = true;
+//							var i_input = input.val().replace(/(,)/g, "");
+//							if (i_input != "" && values_input.val().search("," + i_input + ",") < 0 && i_input.length >= opts.minChars) {
+//								e.preventDefault();
+//								var n_data = {};
+//								n_data[opts.selectedItemProp] = i_input;
+//								n_data[opts.selectedValuesProp] = i_input;
+//								var lis = $("li", selections_holder).length;
+//								add_selected_item(n_data, "00" + (lis + 1));
+//								input.val("");
+//							}
+						case 13: case 9: case 188:  //return tab or comma
 							tab_press = false;
 							var active = $("li.active:first", results_holder);
 							if (active.length > 0) {
