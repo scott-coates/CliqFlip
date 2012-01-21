ca9a3dafe064b8c2e510dcba2b9dcefd41f36d1c
scottc <devnull@localhost>
2012-01-21 10:52:37 -0800

    external tool

---
+++ Solutions/CliqFlip.Infrastructure/Migrator/Migrations/Copy of Migration20120121103924.cs
@@ -0,0 +1,17 @@
+﻿using Migrator.Framework;
+
+namespace CliqFlip.Infrastructure.Migrator.Migrations
+{
+	[Migration(20120121103925)]
+	public class Migration20120sdf121103924 : Migration
+	{
+		public override void Up()
+		{
+			
+		}
+
+		public override void Down()
+		{
+		}
+	}
+}
---
+++ Solutions/CliqFlip.Infrastructure/Migrator/Migrations/Migration20120121103924.cs
@@ -0,0 +1,17 @@
+﻿using Migrator.Framework;
+
+namespace CliqFlip.Infrastructure.Migrator.Migrations
+{
+	[Migration(20120121103924)]
+	public class Migration20120121103924 : Migration
+	{
+		public override void Up()
+		{
+			
+		}
+
+		public override void Down()
+		{
+		}
+	}
+}
