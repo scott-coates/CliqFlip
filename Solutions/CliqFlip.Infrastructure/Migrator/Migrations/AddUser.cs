f3a900a01c6c0926cfd54656af46035ee2b3dd30
scottc <devnull@localhost>
2012-01-21 10:55:32 -0800

    migratsion

---
+++ Solutions/CliqFlip.Infrastructure/Migrator/Migrations/AddUser.cs
@@ -0,0 +1,17 @@
+﻿using Migrator.Framework;
+
+namespace CliqFlip.Infrastructure.Migrator.Migrations
+{
+	[Migration(20120121105512)]
+	public class AddUser : Migration
+	{
+		public override void Up()
+		{
+			
+		}
+
+		public override void Down()
+		{
+		}
+	}
+}
