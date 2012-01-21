971f80d28735d9faaac2577bfc8a6690cc621bd8
scottc <devnull@localhost>
2012-01-21 15:42:58 -0800

    added folder

---
+++ Solutions/CliqFlip.Tests/Generation/Schema/DbSchemaTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CliqFlip.Domain.Entities;
+using CliqFlip.Infrastructure.NHibernateMaps;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace CliqFlip.Tests.Generation.Schema
+{
+	[TestFixture]
+	public class DbSchemaTest
+	{
+		[Test]
+		public void GenerateSchema()
+		{
+			var cfg = new Configuration();
+			cfg.AddAssembly(typeof (User).Assembly);
+			cfg.addcon
+			cfg.Configure();
+
+			new SchemaExport(cfg).Execute(true,true,false);
+		}
+	}
+}
