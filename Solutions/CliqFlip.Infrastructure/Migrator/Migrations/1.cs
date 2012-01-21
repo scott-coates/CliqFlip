2661d3ee9d6ba0e265d9961c5549d2e75644f15c
scottc <devnull@localhost>
2012-01-21 01:52:22 -0800

    auto build

---
+++ Solutions/CliqFlip.Infrastructure/Migrator/Migrations/1.cs
@@ -0,0 +1,17 @@
+﻿using Migrator.Framework;
+
+namespace CliqFlip.Infrastructure.Migrator.Migrations
+{
+	[Migration(20120121005913)]
+	public class Migration1 : Migration
+	{
+		public override void Up()
+		{
+			
+		}
+
+		public override void Down()
+		{
+		}
+	}
+}
