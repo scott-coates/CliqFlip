ca02c07e69fe4525a7ad80f946dce31ed1b22345
scottc <devnull@localhost>
2012-01-23 19:19:38 -0800

    removed

--- Solutions/CliqFlip.SqlServerData/Schema Objects/Database Level Objects/Storage/Files/Test_log.sqlfile.sql
+++
@@ -1,5 +0,0 @@
-﻿ALTER DATABASE [$(DatabaseName)]
-    ADD LOG FILE (NAME = [Test_log], FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\Test_log.ldf', SIZE = 1792 KB, MAXSIZE = 2097152 MB, FILEGROWTH = 10 %);
-
-
-
