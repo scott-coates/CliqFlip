114248defc9de20e59f6e590518297897db51ebb
scottc <devnull@localhost>
2012-01-28 14:18:27 -0800

    profile header stuff

---
+++ Solutions/CliqFlip.Web.Mvc/Content/js/user/profile.js
@@ -0,0 +1,8 @@
+﻿function InitMindMap() {
+	var R = Raphael("mindMap", 500, 500),
+	    c = R.circle(100, 100, 50).attr({
+	    	fill: "hsb(.8, 1, 1)",
+	    	stroke: "none",
+	    	opacity: .5
+	    });
+}
