0ce5ce3e3b3967ec399dfc0a7125b0d0057dacb2
scottc <devnull@localhost>
2012-01-28 17:09:36 -0800

    adding graffle lines

--- Solutions/CliqFlip.Web.Mvc/Content/js/raphael/raphael.cliqFlip.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/raphael/raphael.cliqFlip.js
@@ -1,12 +1,12 @@
 ﻿//http: //raphaeljs.com/ball.html
 //http://raphaeljs.com/reference.html#Raphael.fn
 Raphael.fn.cliqFlip = {
-	mindMapBubble: function (x, y, fill, text) {
-	var c = this.circle(x, y, 70).attr({
-	    	fill: fill,
-	    	stroke: "none",
-	    	opacity: .5
-	    }),
+	mindMapBubble: function (x, y, fill, text, moveCallBack) {
+		var c = this.circle(x, y, 70).attr({
+			fill: fill,
+			stroke: "none",
+			opacity: .5
+		}),
 	    s = this.circle(x + 38, y + 38, 15).attr({
 	    	fill: "hsb(.8, .5, .5)",
 	    	stroke: "none",
@@ -18,23 +18,23 @@ Raphael.fn.cliqFlip = {
 	    	opacity: .5,
 	    	"font-size": "21"
 	    }).toBack();
-	    var start = function () {
-	    	// storing original coordinates
-	    	this.ox = this.attr("cx");
-	    	this.oy = this.attr("cy");
-	    	this.or = this.attr('r');
-	    	this.sizer.ox = this.sizer.attr("cx");
-	    	this.sizer.oy = this.sizer.attr("cy");
-	    	this.text.ox = this.text.attr('x');
-	    	this.text.oy = this.text.attr('y');
-	    	this.attr({
-	    		opacity: 1
-	    	});
-	    	this.sizer.attr({
-	    		opacity: 1
-	    	});
-	    	this.animate({ r: this.or2 + 20, opacity: .25 }, 500, ">");
-	    },
+		var start = function () {
+			// storing original coordinates
+			this.ox = this.attr("cx");
+			this.oy = this.attr("cy");
+			this.or = this.attr('r');
+			this.sizer.ox = this.sizer.attr("cx");
+			this.sizer.oy = this.sizer.attr("cy");
+			this.text.ox = this.text.attr('x');
+			this.text.oy = this.text.attr('y');
+			this.attr({
+				opacity: 1
+			});
+			this.sizer.attr({
+				opacity: 1
+			});
+			this.animate({ r: this.or2 + 20, opacity: .25 }, 500, ">");
+		},
 	    move = function (dx, dy) {
 	    	// move will be called with dx and dy
 	    	this.attr({
@@ -49,6 +49,9 @@ Raphael.fn.cliqFlip = {
 	    		x: this.text.ox + dx,
 	    		y: this.text.oy + dy
 	    	});
+	    	if (moveCallBack) {
+	    		moveCallBack();
+	    	}
 	    },
 	    up = function () {
 	    	// restoring state
@@ -87,15 +90,15 @@ Raphael.fn.cliqFlip = {
 
 	    	}
 	    };
-	    c.drag(move, start, up);
-	    c.sizer = s;
-	    c.text = t;
-	    c.or2 = c.attr('r'); //or2 keeps track of the original raidus when
-	    //the user clicks the bubble and it 'pulses' - prevents the 'fast' double click problem
-	    s.drag(rmove, rstart);
-	    s.big = c;
-	    s.text = t;
+		c.drag(move, start, up);
+		c.sizer = s;
+		c.text = t;
+		c.or2 = c.attr('r'); //or2 keeps track of the original raidus when
+		//the user clicks the bubble and it 'pulses' - prevents the 'fast' double click problem
+		s.drag(rmove, rstart);
+		s.big = c;
+		s.text = t;
 
-		return {big:c,small:s,text:t,paper:this};
+		return { big: c, small: s, text: t, paper: this };
 	}
 };
--- Solutions/CliqFlip.Web.Mvc/Content/js/user/profile.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/user/profile.js
@@ -1,10 +1,21 @@
 ﻿//http://stackoverflow.com/questions/3675519/raphaeljs-drag-n-drop
 function InitMindMap() {
-	Raphael("mindMap", 940, 300)
-		.cliqFlip.mindMapBubble(100,100, "hsb(.25, 1, 1)", "Me\nfirst\nand\nthe\ngimmie\ngimmies")
-		.cliqFlip.mindMapBubble(230,100,"hsb(.09, 1, 1)", "some\nstuff")
-		.cliqFlip.mindMapBubble(360, 100, "hsb(.15, 1, 1)", "some\nstuff")
-		.cliqFlip.mindMapBubble(490, 100, "hsb(1, 1, 1)", "some\nstuff")
-		.cliqFlip.mindMapBubble(620, 100, "hsb(.75, 1, 1)", "some\nstuff")
-		.cliqFlip.mindMapBubble(750,100, "hsb(.07, 1, 1)", "my\ninterest");
+	var r = Raphael("mindMap", 940, 300);
+	var connections = [];
+	var updateConnections = function () {
+		AdjustCollections(r, connections);
+	};
+	var i1 = r.cliqFlip.mindMapBubble(100, 100, "hsb(.25, 1, 1)", "Me\nfirst\nand\nthe\ngimmie\ngimmies",updateConnections);
+	var i2 = r.cliqFlip.mindMapBubble(230, 100, "hsb(.09, 1, 1)", "some\nstuff");
+	connections.push(r.connection(i1.big, i2.big));
+
+	r.cliqFlip.mindMapBubble(360, 100, "hsb(.15, 1, 1)", "some\nstuff");
+	r.cliqFlip.mindMapBubble(490, 100, "hsb(1, 1, 1)", "some\nstuff");
+	r.cliqFlip.mindMapBubble(620, 100, "hsb(.75, 1, 1)", "some\nstuff");
+	r.cliqFlip.mindMapBubble(750, 100, "hsb(.07, 1, 1)", "my\ninterest");
+}
+function AdjustCollections(r,connections) {
+	for (var i = connections.length; i--; ) {
+		r.connection(connections[i]);
+	}
 }
