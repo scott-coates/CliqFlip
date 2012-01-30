8b7bb5318aba043f5a0aa5d4cc79e31f03760da8
scottc <devnull@localhost>
2012-01-30 00:31:30 -0800

    boundary

--- Solutions/CliqFlip.Web.Mvc/Content/js/raphael/raphael.cliqFlip.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/raphael/raphael.cliqFlip.js
@@ -1,24 +1,25 @@
 ﻿//http: //raphaeljs.com/ball.html
 //http://raphaeljs.com/reference.html#Raphael.fn
+//http://stackoverflow.com/questions/3675519/raphaeljs-drag-n-drop
 Raphael.fn.cliqFlip = {
-	mindMapBubble: function (x, y, fill, text) {
-		var c = this.circle(x, y, 70).attr({
+	mindMapBubble: function(x, y, fill, text) {
+		var  c = this.circle(x, y, 70).attr({
 			fill: fill,
 			stroke: "none",
 			opacity: .5
 		}),
-	    s = this.circle(x + 38, y + 38, 15).attr({
-	    	fill: "hsb(.8, .5, .5)",
-	    	stroke: "none",
-	    	opacity: .5
-	    }),
-	    t = this.text(x, y, text).attr({
-	    	fill: "hsb(.2, .2, .2)",
-	    	stroke: "none",
-	    	opacity: .5,
-	    	"font-size": "21"
-	    }).toBack();
-		var start = function () {
+		    s = this.circle(x + 38, y + 38, 15).attr({
+		    	fill: "hsb(.8, .5, .5)",
+		    	stroke: "none",
+		    	opacity: .5
+		    }),
+		    t = this.text(x, y, text).attr({
+		    	fill: "hsb(.2, .2, .2)",
+		    	stroke: "none",
+		    	opacity: .5,
+		    	"font-size": "21"
+		    }).toBack();
+		var start = function() {
 			// storing original coordinates
 			this.ox = this.attr("cx");
 			this.oy = this.attr("cy");
@@ -35,57 +36,62 @@ Raphael.fn.cliqFlip = {
 			});
 			this.animate({ r: this.or2 + 20, opacity: .25 }, 500, ">");
 		},
-	    move = function (dx, dy) {
-	    	// move will be called with dx and dy
-	    	this.attr({
-	    		cx: this.ox + dx,
-	    		cy: this.oy + dy
-	    	});
-	    	this.sizer.attr({
-	    		cx: this.sizer.ox + dx,
-	    		cy: this.sizer.oy + dy
-	    	});
-	    	this.text.attr({
-	    		x: this.text.ox + dx,
-	    		y: this.text.oy + dy
-	    	});
-	    },
-	    up = function () {
-	    	// restoring state
-	    	this.attr({
-	    		opacity: .5
-	    	});
-	    	this.sizer.attr({
-	    		opacity: .5
-	    	});
-	    	this.animate({ r: this.or2, opacity: .5 }, 500, ">");
-	    },
-	    rstart = function () {
-	    	// storing original coordinates
-	    	this.ox = this.attr("cx");
-	    	this.oy = this.attr("cy");
+		    move = function(dx, dy) {
+		    	var newX = this.ox + dx;
+		    	var newY = this.oy + dy;
+		    	if (newX >= 0 && newX <= this.node.parentElement.width.baseVal.value && newY >= 0 && newY <= this.node.parentElement.height.baseVal.value) {
 
-	    	this.big.or = this.big.attr("r");
-	    	this.text.ofs = parseInt(this.text.attr('font-size'), 10);
+		    		// move will be called with dx and dy
+		    		this.attr({
+		    			cx: newX,
+		    			cy: newY
+		    		});
+		    		this.sizer.attr({
+		    			cx: this.sizer.ox + dx,
+		    			cy: this.sizer.oy + dy
+		    		});
+		    		this.text.attr({
+		    			x: this.text.ox + dx,
+		    			y: this.text.oy + dy
+		    		});
+		    	}
+		    },
+		    up = function() {
+		    	// restoring state
+		    	this.attr({
+		    		opacity: .5
+		    	});
+		    	this.sizer.attr({
+		    		opacity: .5
+		    	});
+		    	this.animate({ r: this.or2, opacity: .5 }, 500, ">");
+		    },
+		    rstart = function() {
+		    	// storing original coordinates
+		    	this.ox = this.attr("cx");
+		    	this.oy = this.attr("cy");
 
-	    },
-	    rmove = function (dx, dy) {
-	    	var newR = this.big.or + (dy < 0 ? -1 : 1) * Math.sqrt(2 * dy * dy);
-	    	var newFs = this.text.ofs + (dy < 0 ? -1 : 1) * Math.sqrt(2 * (dy / 2.2) * (dy / 2.2));
+		    	this.big.or = this.big.attr("r");
+		    	this.text.ofs = parseInt(this.text.attr('font-size'), 10);
 
-	    	if (newR <= 130 && newR >= 50) {
-	    		// move will be called with dx and dy
-	    		this.attr({
-	    			cx: this.ox + dy,
-	    			cy: this.oy + dy
-	    		});
-	    		this.big.attr({
-	    			r: newR
-	    		});
-	    		this.big.or2 = newR;
-	    		this.text.attr({ 'font-size': newFs });
-	    	}
-	    };
+		    },
+		    rmove = function(dx, dy) {
+		    	var newR = this.big.or + (dy < 0 ? -1 : 1) * Math.sqrt(2 * dy * dy);
+		    	var newFs = this.text.ofs + (dy < 0 ? -1 : 1) * Math.sqrt(2 * (dy / 2.2) * (dy / 2.2));
+
+		    	if (newR <= 130 && newR >= 50) {
+		    		// move will be called with dx and dy
+		    		this.attr({
+		    			cx: this.ox + dy,
+		    			cy: this.oy + dy
+		    		});
+		    		this.big.attr({
+		    			r: newR
+		    		});
+		    		this.big.or2 = newR;
+		    		this.text.attr({ 'font-size': newFs });
+		    	}
+		    };
 		c.drag(move, start, up);
 		c.sizer = s;
 		c.text = t;
