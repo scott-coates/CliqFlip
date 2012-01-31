19280a176706c47471c94e3f754d2ca799406bb1
scottc <devnull@localhost>
2012-01-30 21:26:07 -0800

    if

--- Solutions/CliqFlip.Web.Mvc/Content/js/user/profile.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/user/profile.js
@@ -1,20 +1,25 @@
 ﻿//http://stackoverflow.com/questions/3675519/raphaeljs-drag-n-drop
 function InitMindMap(interests) {
-	var r = Raphael("mindMap", 940, 300);
-	
-	var initialLayout = interests[0].Passion == null;
-	if (initialLayout) {
-		var rowAxis = 80;
-		var space = 100;
-		var width = 130;
-		for (var interest in interests) {
-			if((parseFloat(interest) + 1) % 7 == 0) {
-				rowAxis += 150;
-				space = 100;
-			}
 
-			r.cliqFlip.mindMapBubble(space, rowAxis, cliqFlip.Utils.RandomHexColor(), interests[interest].Name);
-			space += width;
+	var r = Raphael("mindMap", 940, 300);
+
+	if (interests && interests.length > 0) {
+
+
+		var initialLayout = interests[0].Passion == null;
+		if (initialLayout) {
+			var rowAxis = 80;
+			var space = 100;
+			var width = 130;
+			for (var interest in interests) {
+				if ((parseFloat(interest) + 1) % 7 == 0) {
+					rowAxis += 150;
+					space = 100;
+				}
+
+				r.cliqFlip.mindMapBubble(space, rowAxis, cliqFlip.Utils.RandomHexColor(), interests[interest].Name);
+				space += width;
+			}
 		}
 	}
 }
