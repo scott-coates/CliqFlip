275d970d74f79352ef8e67d7d03df75ec436b05a
scottc <devnull@localhost>
2012-01-31 23:29:54 -0800

    can now add it

---
+++ Solutions/CliqFlip.Web.Mvc/Content/js/cliqFlip/utils.js
@@ -0,0 +1,15 @@
+﻿//http://stackoverflow.com/questions/2504568/javascript-namespace-declaration
+var cliqFlip = (function(cliqFlip) {
+	cliqFlip.Utils = { };
+
+	cliqFlip.Utils.RandomHexColor = function() {
+//		http: //css-tricks.com/snippets/javascript/random-hex-color/#comment-83815
+		var x = Math.round(0xffffff * Math.random()).toString(16);
+		var y = (6 - x.length);
+		var z = '000000';
+		var z1 = z.substring(0, y);
+		var color = '#' + z1 + x;
+		return color;
+	};
+	return cliqFlip;
+}(cliqFlip || { }));
