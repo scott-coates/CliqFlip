2e4d7bed9de19ce8b2fd3434944e5aba2704dad4
Jorge <Jorge@Jorge-PC>
2012-02-01 21:59:13 -0800

    add the user interest coordinates to the db using migrations

---
+++ Solutions/CliqFlip.Infrastructure/Migrator/Migrations/__634637297459282121_AddUserInterestCoordinates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Migrator.Framework;
+
+namespace CliqFlip.Infrastructure.Migrator.Migrations
+{
+    [Migration(634637297459282121)]
+    public class __634637297459282121_AddUserInterestCoordinates : Migration
+    {
+        public override void Up()
+        {
+            Database.AddColumn("UserInterests", "XAxis", System.Data.DbType.Double, ColumnProperty.Null);
+            Database.AddColumn("UserInterests", "YAxis", System.Data.DbType.Double, ColumnProperty.Null); 
+        }
+        public override void Down()
+        {
+            Database.RemoveColumn("UserInterests", "XAxis");
+            Database.RemoveColumn("UserInterests", "YAxis");
+        }
+    }
+}
