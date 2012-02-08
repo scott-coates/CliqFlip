89e087f0106c80c14f8e6f546b0a8e948744a19f
Jorge <Jorge@Jorge-PC>
2012-02-08 15:18:53 -0800

    moved FormsAuth to service

---
+++ Solutions/CliqFlip.Web.Mvc/Extensions/Authentication/IUserAuthentication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CliqFlip.Web.Mvc.Extensions.Authentication
+{
+    public interface IUserAuthentication
+    {
+        void Login(string username, bool stayLoggedIn);
+        void Logout();
+    }
+}
