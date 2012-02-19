9a0442911a4b2401867b31da3b22ce307fca9ba6
scottc <devnull@localhost>
2012-02-19 12:47:56 -0800

    test file upload class

---
+++ Solutions/CliqFlip.Infrastructure/IO/FileUploadService.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.IO;
+using CliqFlip.Infrastructure.IO.Interfaces;
+
+namespace CliqFlip.Infrastructure.IO
+{
+	public class TestFileUpload : IFileUploadService
+	{
+		public IList<string> UploadFiles(string path, IList<FileToUpload> files)
+		{
+			var retVal = new List<string>();
+			foreach(var file in files)
+			{
+				var saveFilePath = "C:\\test\\" + path + file.Filename;
+				using (FileStream fileStream = File.Create(saveFilePath))
+				{
+					file.Stream.CopyTo(fileStream);
+				}
+
+				retVal.Add(saveFilePath);
+			}
+
+			return retVal;
+		}
+
+		public void DeleteFiles(string path, IList<string> fileNames)
+		{
+			
+		}
+	}
+}
