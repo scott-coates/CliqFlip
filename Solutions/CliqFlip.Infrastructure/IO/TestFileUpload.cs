eab0224fa4d97f70e8742ba76b7e4ffcdbce2ccf
scottc <devnull@localhost>
2012-02-19 23:53:42 -0800

    hide file

--- Solutions/CliqFlip.Infrastructure/IO/TestFileUpload.cs
+++ Solutions/CliqFlip.Infrastructure/IO/TestFileUpload.cs
@@ -1,31 +1,31 @@
-using System.Collections.Generic;
-using System.IO;
-using CliqFlip.Infrastructure.IO.Interfaces;
+//using System.Collections.Generic;
+//using System.IO;
+//using CliqFlip.Infrastructure.IO.Interfaces;
 
-namespace CliqFlip.Infrastructure.IO
-{
-	public class TestFileUpload : IFileUploadService
-	{
-		public IList<string> UploadFiles(string path, IList<FileToUpload> files)
-		{
-			var retVal = new List<string>();
-			foreach(var file in files)
-			{
-				var saveFilePath = "C:\\test\\" + path + file.Filename;
-				using (FileStream fileStream = File.Create(saveFilePath))
-				{
-					file.Stream.CopyTo(fileStream);
-				}
+//namespace CliqFlip.Infrastructure.IO
+//{
+//    public class TestFileUpload : IFileUploadService
+//    {
+//        public IList<string> UploadFiles(string path, IList<FileToUpload> files)
+//        {
+//            var retVal = new List<string>();
+//            foreach(var file in files)
+//            {
+//                var saveFilePath = "C:\\test\\" + path + file.Filename;
+//                using (FileStream fileStream = File.Create(saveFilePath))
+//                {
+//                    file.Stream.CopyTo(fileStream);
+//                }
 
-				retVal.Add(saveFilePath);
-			}
+//                retVal.Add(saveFilePath);
+//            }
 
-			return retVal;
-		}
+//            return retVal;
+//        }
 
-		public void DeleteFiles(string path, IList<string> fileNames)
-		{
+//        public void DeleteFiles(string path, IList<string> fileNames)
+//        {
 			
-		}
-	}
-}
+//        }
+//    }
+//}
