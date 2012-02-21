fc7d03af5236c0d576a5e6aac35be6d9395b71d3
scottc <devnull@localhost>
2012-02-20 22:29:41 -0800

    commit

--- Solutions/CliqFlip.Web.Mvc/Content/js/global.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/global.js
@@ -2,25 +2,32 @@
 
 
 function formLogin_OnSuccess(result, response) {
-    //if the result does not contain the word failed
-    if (result.indexOf("failed") == -1) {
-        window.location.reload();
-    }
+	//if the result does not contain the word failed
+	if (result.indexOf("failed") == -1) {
+		window.location.reload();
+	}
 }
 
 
-
 function InitLogin() {
-    var userLoginContainer = $("#user-login-container");
-    $("#user-login").click(function (event) {
-    	event.preventDefault();
-    	$(this).toggleClass("selected");
-    	userLoginContainer.toggle();
-    	$("#Username").focus();
-    });
+	var userLoginContainer = $("#user-login-container");
+	userLoginContainer.keydown(function(event) {
 
-    $("#close-login").click(function (event) {
-    	event.preventDefault();
-    	userLoginContainer.hide();
-    });
+		// ESCAPE key pressed
+		if (event.keyCode == 27) {
+			userLoginContainer.toggle();
+		}
+	});
+
+	$("#user-login").click(function(event) {
+		event.preventDefault();
+		$(this).toggleClass("selected");
+		userLoginContainer.toggle();
+		$("#Username").focus();
+	});
+
+	$("#close-login").click(function(event) {
+		event.preventDefault();
+		userLoginContainer.hide();
+	});
 }
