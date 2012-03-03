8c207ce8698d5452de97f7ebf86cf8b429f2d131
Scott Coates <devnull@localhost>
2012-03-03 15:33:50 -0800

    test raised exception

--- Solutions/CliqFlip.Web.Mvc/Areas/Admin/Controllers/TestController.cs
+++ Solutions/CliqFlip.Web.Mvc/Areas/Admin/Controllers/TestController.cs
@@ -1,12 +1,20 @@
 using System;
 using System.Web;
 using System.Web.Mvc;
+using CliqFlip.Infrastructure.Logging.Interfaces;
 
 namespace CliqFlip.Web.Mvc.Areas.Admin.Controllers
 {
 	[Authorize(Users = "scott")]
 	public class TestController : Controller
 	{
+		private readonly ILogger _logger;
+
+		public TestController(ILogger logger)
+		{
+			_logger = logger;
+		}
+
 		public ActionResult Index()
 		{
 			return new EmptyResult();
@@ -20,6 +28,12 @@ namespace CliqFlip.Web.Mvc.Areas.Admin.Controllers
 		public ActionResult RandomError()
 		{
 			throw new HttpException(500, "Test Error " + new Random().NextDouble());
+		}
+
+		public ActionResult LogError()
+		{
+			_logger.LogException(new Exception("Raised Execpton"));
+			return new EmptyResult();
 		}
 	}
 }
