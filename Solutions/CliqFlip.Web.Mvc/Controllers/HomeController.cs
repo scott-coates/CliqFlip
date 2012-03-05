db83500fe94826a64f817845bcef8a950121c285
Scott Coates <devnull@localhost>
2012-03-05 00:56:57 -0800

    comment & order by name

--- Solutions/CliqFlip.Web.Mvc/Controllers/HomeController.cs
+++ Solutions/CliqFlip.Web.Mvc/Controllers/HomeController.cs
@@ -17,18 +17,20 @@ namespace CliqFlip.Web.Mvc.Controllers
 		public ActionResult Index()
 		{
 			var viewModel = new IndexViewModel
-			                	{
-			                		KeywordSearchUrl = "\"" + Url.Action("Interest", "Search") + "\"",
-			                		TagCloudInterests = _interestTasks
-			                			.GetMostPopularInterests()
-			                			.Select(x => new IndexViewModel.TagCloudInterestsViewModel
+								{
+									//TODO: Put this in a query - like how we do with the user controller
+									KeywordSearchUrl = "\"" + Url.Action("Interest", "Search") + "\"",
+									TagCloudInterests = _interestTasks
+										.GetMostPopularInterests()
+										.OrderBy(x => x.Name)
+										.Select(x => new IndexViewModel.TagCloudInterestsViewModel
 
-			                			             	{
-			                			             		Name = x.Name,
-			                			             		Slug = x.Slug,
-			                			             		Weight = x.Count
-			                			             	}).ToList()
-			                	};
+														{
+															Name = x.Name,
+															Slug = x.Slug,
+															Weight = x.Count
+														}).ToList()
+								};
 
 			return View(viewModel);
 		}
