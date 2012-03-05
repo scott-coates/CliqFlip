ea177e4e72da832d7c0238ddfc64af0393650b5a
Scott Coates <devnull@localhost>
2012-03-05 00:46:44 -0800

    added send email test

---
+++ Solutions/CliqFlip.Tests/Integration/Email/SESEmailTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CliqFlip.Domain.Contracts.Tasks;
+using CliqFlip.Domain.Entities;
+using CliqFlip.Domain.Exceptions;
+using CliqFlip.Infrastructure.Email;
+using CliqFlip.Infrastructure.Email.Interfaces;
+using CliqFlip.Infrastructure.Syndication.Interfaces;
+using CliqFlip.Infrastructure.Web.Interfaces;
+using CliqFlip.Tasks.TaskImpl;
+using Moq;
+using NUnit.Framework;
+
+namespace CliqFlip.Tests.Integration.Email
+{
+	[TestFixture]
+	public class SESEmailTests
+	{
+		private IEmailService _emailService;
+
+		[SetUp]
+		public void Setup()
+		{
+			_emailService = new SESEmailService();
+		}
+
+		#region Email tests
+
+		[Ignore]
+		[Test]
+		public void SendEmail()
+		{
+			Assert.Pass("Send email");
+		}
+
+		#endregion
+
+	}
+}
