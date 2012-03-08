277ad4247f5ea56c9f411e97ea77d5546a781147
Jorge <Jorge@Jorge-PC>
2012-03-07 23:26:00 -0800

    forgot to select migration

---
+++ Solutions/CliqFlip.Web.Mvc/ViewModels/User/UserSendMessageToViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CliqFlip.Web.Mvc.ViewModels.User
+{
+    public class UserSendMessageToViewModel
+    {
+    }
+}
