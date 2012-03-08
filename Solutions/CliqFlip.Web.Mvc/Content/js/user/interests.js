390c05e1ca28b365d5bf255a763032be753a96f6
Scott Coates <devnull@localhost>
2012-03-08 14:06:56 -0800

    external

--- Solutions/CliqFlip.Web.Mvc/Content/js/user/interests.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/user/interests.js
@@ -13,9 +13,29 @@ function InitSaveImages() {
 			width: 430
 		});
 
-		$(".user-interest-title-add-photo").click(function (parameters) {
+		$(".user-interest-title-add-photo").click(function(parameters) {
 			$("#userInterestId").val($(this).attr('value'));
 			_addPhotoDialog.dialog("open");
 		});
 	}
+}
+
+function InitShowImages() {
+	$('.intereset-slider').anythingSlider(
+		{
+			hashTags: false,
+			resizeContents: false,
+			theme: "default1",
+			buildStartStop: false
+		})
+		.show()
+		.find('.panel:not(.cloned) a') // ignore the cloned panels
+		.colorbox({
+			width: '90%',
+			height: '90%',
+			preloading: false,
+			title: function() {
+				return "To view full size, " + "click here!".link(this.href);
+			}
+		});
 }
