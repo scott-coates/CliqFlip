d316f38fa66f0a6aa8690a26b0738efc13493568
Scott Coates <devnull@localhost>
2012-03-10 11:07:29 -0800

    new mind maps are positioned corectly

--- Solutions/CliqFlip.Web.Mvc/Content/js/user/profile.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/user/profile.js
@@ -12,18 +12,25 @@ function InitMindMap(interests, saveUrl) {
 	var r = Raphael("mindMap", 559, 300);
 
 	if (interests && interests.length > 0) {
-
-		var initialLayout = !interests[0].Passion;
-		if (initialLayout) {
-			var rowAxis = 80;
-			var space = 100;
-			var width = 130;
-			for (var interest in interests) {
+		var rowAxis = 80;
+		var space = 100;
+		var width = 130;
+		for (var interest in interests) {
+			if (interests[interest].Passion) {
+				_bubbles.push(r.cliqFlip.createMindMapBubble(interests[interest].Passion,
+					interests[interest].XAxis,
+					interests[interest].YAxis,
+					cliqFlip.Ui.RandomHexColor(),
+					interests[interest].Name,
+					interests[interest].Id,
+					_canEdit,
+					MindMapSave));
+			}
+			else {
 				if ((parseFloat(interest) + 1) % 7 == 0) {
 					rowAxis += 150;
 					space = 100;
 				}
-
 				_bubbles.push(r.cliqFlip.createMindMapBubble(2.5,
 					space,
 					rowAxis,
@@ -34,18 +41,6 @@ function InitMindMap(interests, saveUrl) {
 					MindMapSave));
 
 				space += width;
-
-			}
-		} else {
-			for (interest in interests) {
-				_bubbles.push(r.cliqFlip.createMindMapBubble(interests[interest].Passion,
-					interests[interest].XAxis,
-					interests[interest].YAxis,
-					cliqFlip.Ui.RandomHexColor(),
-					interests[interest].Name,
-					interests[interest].Id,
-					_canEdit,
-					MindMapSave));
 			}
 		}
 	}
@@ -59,7 +54,7 @@ function MindMapSave(mindMapObj) {
 		passion: mindMapObj.GetPassion()
 	};
 
-	$.post(_mindMapSaveUrl, interest, function (data) {
+	$.post(_mindMapSaveUrl, interest, function(data) {
 		var jqObj = $("#saveMindMapText");
 		cliqFlip.Ui.Blink(jqObj);
 	}, "json");
@@ -101,38 +96,43 @@ function InitWebsiteUrl(saveUrl) {
 }
 
 function InitFacebook() {
-    $("#facebook-connect").click(function (event) {
-        var p = $(this).find("p");
-        FB.login(function (response) {
-            if (response.authResponse) {
-                //send the user id to our app
-                $.post("/User/SaveFacebook", { fbid: response.authResponse.userID }, function () {
-                    p.html("Linked...");
-                });
-            } else {
-                //TODO: tell the user something wen't wrong
-            }
-        });
-    });
+	$("#facebook-connect").click(function(event) {
+		var p = $(this).find("p");
+		FB.login(function(response) {
+			if (response.authResponse) {
+				//send the user id to our app
+				$.post("/User/SaveFacebook", { fbid: response.authResponse.userID }, function() {
+					p.html("Linked...");
+				});
+			}
+			else {
+				//TODO: tell the user something wen't wrong
+			}
+		});
+	});
 }
 
 function InitFacebookSdk(appId) {
-    window.fbAsyncInit = function () {
-        FB.init({
-            appId: appId, // App ID
-            //channelUrl: '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
-            status: true, // check login status
-            cookie: true, // enable cookies to allow the server to access the session
-            xfbml: true  // parse XFBML
-        });
-    };
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId: appId, // App ID
+			//channelUrl: '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
+			status: true, // check login status
+			cookie: true, // enable cookies to allow the server to access the session
+			xfbml: true  // parse XFBML
+		});
+	};
 
-    // Load the SDK Asynchronously
-    (function (d) {
-        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement('script'); js.id = id; js.async = true;
-        js.src = "//connect.facebook.net/en_US/all.js";
-        ref.parentNode.insertBefore(js, ref);
-    } (document));
+	// Load the SDK Asynchronously
+	(function(d) {
+		var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+		if (d.getElementById(id)) {
+			return;
+		}
+		js = d.createElement('script');
+		js.id = id;
+		js.async = true;
+		js.src = "//connect.facebook.net/en_US/all.js";
+		ref.parentNode.insertBefore(js, ref);
+	}(document));
 }
