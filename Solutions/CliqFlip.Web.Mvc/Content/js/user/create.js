f59cd6b24a432d37584e89b3d08070a8c538a219
Scott Coates <devnull@localhost>
2012-03-09 21:45:01 -0800

    splittting up create screen

--- Solutions/CliqFlip.Web.Mvc/Content/js/user/create.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/user/create.js
@@ -1,178 +1,184 @@
 ﻿//the textbox the user types their interest into
-var interestTextBox = null;
+var _interestTextBox = null;
+
 //the div holding the category questions
-var divInterestCategory = null;
+var _divInterestCategory = null;
 
 //the create form
-var formProfileCreate = null;
+var _formProfileCreate = null;
 
 //the list of added interests
-var interestsList = null;
+var _interestsList = null;
 
-var tmplNewInterest = null;
-var tmplStepIndicator = null;
-var steps = null;
+var _tmplNewInterest = null;
+var _tmplStepIndicator = null;
+var _steps = null;
 
-var interestNames = new Array();
-//TODO: use [] instead of array obj
+var _interestNames = [];
 
-function initUserCreate() {
-	var selectedCssClass = "dark-blue";
-	var unselectedCssClass = "blue";
+var _selectedCssClass = "dark-blue";
+var _unselectedCssClass = "blue";
 
-	function generateIndicators() {
-		//each fieldset is a step in in the wizard.
-		//The <strong> tag will serve all the title
-		//add an indicator for each step in the wizard
-		formProfileCreate.find("fieldset").each(function(index, element) {
-			var title = $(this).find("strong").text();
-			var stepId = $(this).attr("id");
-			var indicator = tmplStepIndicator.tmpl({
-				StepNumber: index + 1,
-				StepTitle: title,
-				StepId: stepId
-			});
-			if (index == 0) {
-				indicator.find("a").addClass(selectedCssClass).removeClass(unselectedCssClass);
-			}
-			indicator.appendTo(steps);
-		});
+function InitUserCreate() {
+	//bind all the elements we need
+	_interestTextBox = $("#interestName");
+	_divInterestCategory = $("#_divInterestCategory");
+	_interestsList = $("#_interestsList");
+	_tmplNewInterest = $("#_tmplNewInterest");
+	_tmplStepIndicator = $("#_tmplStepIndicator");
+	_steps = $("#_steps");
 
-		//bind the indicators to move to the step whenever it's clicked
-		steps.find("a").click(function(event) {
-			event.preventDefault();
-			var step = $(this).attr("href").replace("#", "");
-			formProfileCreate.formwizard("show", step);
-		});
+
+	//find the user create form
+	_formProfileCreate = $("#_formProfileCreate");
+
+	CreateFormWizard();
+	GenerateIndicators();
+	CreateAutoComplete();
+
+
+}
+
+function GenerateVisibility(interest) {
+	if (interest.Id != 0) {
+		return "visibility: hidden;";
 	}
+	return '';
+}
 
-	function createFormWizard() {
-		var onStepShown = function(event, data) {
-
-			//Whenever a step is show, update the indicators to reflect the step
-			//their looking at.
-			//get all the indicators with the selectedCssClass and remove the class
-			//TODO: remove the contains selector
-			steps.find("a[class~='" + selectedCssClass + "']").removeClass(selectedCssClass).addClass(unselectedCssClass);
-
-			//mark the currently selected step
-			steps.find("a[href='#" + data.currentStep + "']").addClass(selectedCssClass).removeClass(unselectedCssClass);
-		};
-
-		var formWizardOptions = {
-			disableUIStyles: true,
-			inDuration: 100,
-			outDuration: 100
-		};
-
-		formProfileCreate.formwizard(formWizardOptions).bind("step_shown", onStepShown);
-		formProfileCreate.data('validator').settings.submitHandler = function(form) {
-			var userInterests = $(".userInterestValue", $(form));
-			if (userInterests.length > 0) {
-				form.submit();
-			} else {
-				formProfileCreate.formwizard("show", "stepInterests");
-				$("#userInterestEmptyError").fadeIn(1000);
-			}
-		};
-	}
-
-	function createAutoComplete() {
-		interestTextBox.autocomplete({
-			source: function(request, response) {
-				// delegate back to autocomplete, but extract the last term
-				var term = request.term;
-
-				var addToList = true;
-
-				//make a call to the Interest search
-				//If the item the user has typed does not have an exact match
-				//it should add it to the end of the list by pushing a new json object into the Interest array
-				//Then let autocomplete do it's thing
-				$.getJSON("/Search/Interest?input=" + term, function(data) {
-
-					//if the item the user is searching for is in the list
-					//it should not be added at the end
-					for (var counter = 0; counter < data.length; counter++) {
-						if (data[counter].Name.toLowerCase() == term.toLowerCase()) {
-							addToList = false;
-						}
-					}
-
-					if (addToList) {
-						data.push({
-							Name: term,
-							Id: 0
-						});
-					}
-					response(data);
-				});
-			},
-			select: function(event, ui) {
-				onInterestAdded(ui.item);
-				this.value = "";
-				return false;
-			}
-		}).data("autocomplete")._renderItem = function(ul, item) {
-			return $("<li></li>").data("item.autocomplete", item).append("<a>" + item.Name + "</a>").appendTo(ul);
-		};
-
-		//whenever the link 'a.remove-interest' is clicked
-		//it should remove the interest from the list of added interests
-		interestsList.on("click", "a.remove-interest", function(event) {
-			event.preventDefault();
-			var li = $(this) //the anchor clicked
-				.parents("li"); //get the closest parent li
-			var interest = li.data("item");
-
-			onInterestRemoved(interest);
-
-			li.fadeOut(function() { //fade it out
-				li.remove(); //then remove it
-			});
+function GenerateIndicators() {
+	//each fieldset is a step in in the wizard.
+	//The <strong> tag will serve all the title
+	//add an indicator for each step in the wizard
+	_formProfileCreate.find("fieldset").each(function(index, element) {
+		var title = $(this).find("strong").text();
+		var stepId = $(this).attr("id");
+		var indicator = _tmplStepIndicator.tmpl({
+			StepNumber: index + 1,
+			StepTitle: title,
+			StepId: stepId
 		});
-	}
-
-	function onInterestAdded(interest) {
-
-		//if an interest with the same Interest has been added
-		if ($.inArray(interest.Name.toLowerCase(), interestNames) >= 0) {
-			return;
+		if (index == 0) {
+			indicator.find("a").addClass(_selectedCssClass).removeClass(_unselectedCssClass);
 		}
-
-		interestNames.push(interest.Name.toLowerCase());
-		var data = $.extend({ }, {
-			Index: interestsList.find("li").length
-		}, interest);
-		var newItem = tmplNewInterest.tmpl(data);
-		newItem.data("item", interest);
-		newItem.appendTo(interestsList);
-		newItem.fadeIn();
-	}
-
-	function onInterestRemoved(interest) {
-		interestNames = $.grep(interestNames, function(e) {
-			return e !== interest.Name.toLowerCase();
-		});
-	}
-
-	$(document).ready(function() {
-		//TODO:we should pick out a naming conventions for js functions - camel case or pascal case
-		//TODO: we should determine if doc ready function should be called in js files or in aspx files
-		//bind all the elements we need
-		interestTextBox = $("#interestName");
-		divInterestCategory = $("#divInterestCategory");
-		interestsList = $("#interestsList");
-		tmplNewInterest = $("#tmplNewInterest");
-		tmplStepIndicator = $("#tmplStepIndicator");
-		steps = $("#steps");
-
-
-		//find the user create form
-		formProfileCreate = $("#formProfileCreate");
-
-		createFormWizard();
-		generateIndicators();
-		createAutoComplete();
+		indicator.appendTo(_steps);
 	});
+
+	//bind the indicators to move to the step whenever it's clicked
+	_steps.find("a").click(function(event) {
+		event.preventDefault();
+		var step = $(this).attr("href").replace("#", "");
+		_formProfileCreate.formwizard("show", step);
+	});
+}
+
+function CreateAutoComplete() {
+	_interestTextBox.autocomplete({
+		source: function(request, response) {
+			// delegate back to autocomplete, but extract the last term
+			var term = request.term;
+
+			var addToList = true;
+
+			//make a call to the Interest search
+			//If the item the user has typed does not have an exact match
+			//it should add it to the end of the list by pushing a new json object into the Interest array
+			//Then let autocomplete do it's thing
+			$.getJSON("/Search/Interest?input=" + term, function(data) {
+
+				//if the item the user is searching for is in the list
+				//it should not be added at the end
+				for (var counter = 0; counter < data.length; counter++) {
+					if (data[counter].Name.toLowerCase() == term.toLowerCase()) {
+						addToList = false;
+					}
+				}
+
+				if (addToList) {
+					data.push({
+						Name: term,
+						Id: 0
+					});
+				}
+				response(data);
+			});
+		},
+		select: function(event, ui) {
+			OnInterestAdded(ui.item);
+			this.value = "";
+			return false;
+		}
+	}).data("autocomplete")._renderItem = function(ul, item) {
+		return $("<li></li>").data("item.autocomplete", item).append("<a>" + item.Name + "</a>").appendTo(ul);
+	};
+
+	//whenever the link 'a.remove-interest' is clicked
+	//it should remove the interest from the list of added interests
+	_interestsList.on("click", "a.remove-interest", function(event) {
+		event.preventDefault();
+		var li = $(this) //the anchor clicked
+			.parents("li"); //get the closest parent li
+		var interest = li.data("item");
+
+		OnInterestRemoved(interest);
+
+		li.fadeOut(function() { //fade it out
+			li.remove(); //then remove it
+		});
+	});
+}
+
+function OnInterestAdded(interest) {
+
+	//if an interest with the same Interest has been added
+	if ($.inArray(interest.Name.toLowerCase(), _interestNames) >= 0) {
+		return;
+	}
+
+	_interestNames.push(interest.Name.toLowerCase());
+	var data = $.extend({ }, {
+		Index: _interestsList.find("li").length
+	}, interest);
+	var newItem = _tmplNewInterest.tmpl(data);
+	newItem.data("item", interest);
+	newItem.appendTo(_interestsList);
+	newItem.fadeIn();
+}
+
+
+function OnInterestRemoved(interest) {
+	_interestNames = $.grep(_interestNames, function(e) {
+		return e !== interest.Name.toLowerCase();
+	});
+}
+
+function CreateFormWizard() {
+	var onStepShown = function(event, data) {
+
+		//Whenever a step is show, update the indicators to reflect the step
+		//their looking at.
+		//get all the indicators with the selectedCssClass and remove the class
+		//TODO: remove the contains selector
+		_steps.find("a[class~='" + _selectedCssClass + "']").removeClass(_selectedCssClass).addClass(_unselectedCssClass);
+
+		//mark the currently selected step
+		_steps.find("a[href='#" + data.currentStep + "']").addClass(_selectedCssClass).removeClass(_unselectedCssClass);
+	};
+
+	var formWizardOptions = {
+		disableUIStyles: true,
+		inDuration: 100,
+		outDuration: 100
+	};
+
+	_formProfileCreate.formwizard(formWizardOptions).bind("step_shown", onStepShown);
+	_formProfileCreate.data('validator').settings.submitHandler = function(form) {
+		var userInterests = $(".userInterestValue", $(form));
+		if (userInterests.length > 0) {
+			form.submit();
+		} else {
+			_formProfileCreate.formwizard("show", "stepInterests");
+			$("#userInterestEmptyError").fadeIn(1000);
+		}
+	};
 }
