39fdfd80dc69acb0e5426b61cb9081251fae920d
Scott Coates <devnull@localhost>
2012-03-20 19:15:52 -0700

    major locations and stuff

---
+++ Solutions/CliqFlip.Domain/Entities/MajorLocation.cs
@@ -0,0 +1,20 @@
+﻿using SharpArch.Domain.DomainModel;
+
+namespace CliqFlip.Domain.Entities
+{
+	public class MajorLocation : Entity
+	{
+		public virtual string Slug { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string Country { get; set; }
+		public virtual string TimeZone { get; set; }
+		public virtual string TimeZoneOffsetInSeconds { get; set; }
+		public virtual string Latitude { get; set; }
+		public virtual string Longitude { get; set; }
+		public virtual string SlugSpecificSubset { get; set; }
+		public virtual string NameSpecificSubset { get; set; }
+		public virtual string Name3 { get; set; }
+		public virtual string LatitudeSpecificSubset { get; set; }
+		public virtual string LongitudeSpecificSubset { get; set; }
+	}
+}
