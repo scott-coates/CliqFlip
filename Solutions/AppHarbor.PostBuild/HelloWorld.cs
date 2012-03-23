79e458a005e21b5fed2107628d775f058a2d8338
Scott Coates <devnull@localhost>
2012-03-23 02:38:16 -0700

    pre and post

---
+++ Solutions/AppHarbor.PostBuild/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace CustomTask
+{
+	public class HelloWorld : Task
+	{
+		public override bool Execute()
+		{
+			Log.LogMessage(MessageImportance.High, "Inside Custom Task.");
+			return true;
+		}
+
+		[Output]
+		public string Test
+		{
+			get
+			{
+				return "Hello, World!";
+			}
+		}
+	}
+}
---
+++ Solutions/AppHarbor.PreBuild/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppHarbor.Build
+{
+	public class Program
+	{
+		public static void Main()
+		{
+		}
+	}
+}
