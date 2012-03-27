60447bd67d1b6b4f4a9f61326efb376f914c663c
Jorge <Jorge@Jorge-PC>
2012-03-27 03:39:31 -0700

    moved css and js to appropriate places

---
+++ Solutions/CliqFlip.Web.Mvc/Content/js/search/index.js
@@ -0,0 +1,7 @@
+﻿function InitSearchPage() {
+    $("div.userSearchBio").dotdotdot();
+    $("div.innerUserSearchResults").click(function (event) {
+        window.location =
+                $(this).find("a.user-profile").attr("href");
+    });
+}
