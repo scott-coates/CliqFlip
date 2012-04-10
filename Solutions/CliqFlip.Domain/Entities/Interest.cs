ac9cb320aedec90a42077094b454646052196c88
Jorge <Jorge@Jorge-PC>
2012-04-09 22:49:59 -0700

    added IsMainCategory to Interest and created migration

--- Solutions/CliqFlip.Domain/Entities/Interest.cs
+++ Solutions/CliqFlip.Domain/Entities/Interest.cs
@@ -11,6 +11,7 @@ namespace CliqFlip.Domain.Entities
 		public virtual string Name { get; set; }
 		public virtual string Description { get; set; }
 		public virtual string Slug { get; set; }
+        public virtual bool IsMainCategory { get; set; }
 
 		public Interest()
 		{
---
+++ Solutions/CliqFlip.Infrastructure/Migrator/Migrations/__634696078341536810_AddIsMainCategoryColumn.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using Migrator.Framework;
+
+namespace CliqFlip.Infrastructure.Migrator.Migrations
+{
+    [Migration(634696078341536810)]
+    public class __634696078341536810_AddIsMainCategoryColumn : Migration
+	{
+		public override void Up()
+		{
+			Database.AddColumn("Interests", "IsMainCategory", DbType.Boolean, 1, ColumnProperty.NotNull, false);
+		}
+
+		public override void Down()
+		{
+            Database.RemoveColumn("Interests", "IsMainCategory");
+		}
+	}
+}
