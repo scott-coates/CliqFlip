1aaeb629e4378997bab2358a90a4bdf36525e412
Scott Coates <devnull@localhost>
2012-04-10 19:56:26 -0700

    with versioning

--- Common/AssemblyVersion.cs
+++ Common/AssemblyVersion.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 
 
