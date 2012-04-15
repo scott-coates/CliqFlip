786e42672b244ba6100e8490c19389243d4755fa
Scott Coates <devnull@localhost>
2012-04-15 01:08:37 -0700

    Backed out changeset: 5b40f519aed0

--- Solutions/CliqFlip.Web.Mvc/Security/Attributes/FormsAuthReadUserDataAttribute.cs
+++ Solutions/CliqFlip.Web.Mvc/Security/Attributes/FormsAuthReadUserDataAttribute.cs
@@ -3,7 +3,6 @@ using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
-using CliqFlip.Infrastructure.Logging;
 
 namespace CliqFlip.Web.Mvc.Security.Attributes
 {
@@ -22,11 +21,9 @@ namespace CliqFlip.Web.Mvc.Security.Attributes
 				FormsAuthenticationTicket ticket = identity.Ticket;
 
 				var principal = new GenericPrincipal(identity, new[] { ticket.UserData });
-				new ElmahLogger().LogException(new Exception("principal " + ticket.UserData));
+
 				httpContext.User = principal;
 			}
-
-			new ElmahLogger().LogException(new Exception("is authorized " + isAuthorized));
 			return isAuthorized;
 		}
 
