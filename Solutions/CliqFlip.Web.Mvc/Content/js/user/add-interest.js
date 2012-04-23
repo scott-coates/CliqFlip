309393a3548d40ebec61381b0a1eeeeabecabc09
Jorge <Jorge@Jorge-PC>
2012-04-23 08:20:48 -0700

    updated jquery ui added interest autocomplete autoFocus

--- Solutions/CliqFlip.Web.Mvc/Content/js/user/add-interest.js
+++ Solutions/CliqFlip.Web.Mvc/Content/js/user/add-interest.js
@@ -56,44 +56,45 @@ function SetupEnterKey() {
 }
 
 function CreateAutoComplete() {
-	_interestTextBox.autocomplete({
-		source: function(request, response) {
-			// delegate back to autocomplete, but extract the last term
-			var term = request.term;
+    _interestTextBox.autocomplete({
+        source: function (request, response) {
+            // delegate back to autocomplete, but extract the last term
+            var term = request.term;
 
-			var addToList = true;
+            var addToList = true;
 
-			//make a call to the Interest search
-			//If the item the user has typed does not have an exact match
-			//it should add it to the end of the list by pushing a new json object into the Interest array
-			//Then let autocomplete do it's thing
-			$.getJSON("/Search/Interest?input=" + term, function(data) {
+            //make a call to the Interest search
+            //If the item the user has typed does not have an exact match
+            //it should add it to the end of the list by pushing a new json object into the Interest array
+            //Then let autocomplete do it's thing
+            $.getJSON("/Search/Interest?input=" + term, function (data) {
 
-				//if the item the user is searching for is in the list
-				//it should not be added at the end
-				for (var counter = 0; counter < data.length; counter++) {
-					if (data[counter].Name.toLowerCase() == term.toLowerCase()) {
-						addToList = false;
-					}
-				}
+                //if the item the user is searching for is in the list
+                //it should not be added at the end
+                for (var counter = 0; counter < data.length; counter++) {
+                    if (data[counter].Name.toLowerCase() == term.toLowerCase()) {
+                        addToList = false;
+                    }
+                }
 
-				if (addToList) {
-					data.push({
-						Name: term,
-						Id: 0
-					});
-				}
-				response(data);
-			});
-		},
-		select: function(event, ui) {
-			OnInterestAdded(ui.item);
-			this.value = "";
-			return false;
-		}
-	}).data("autocomplete")._renderItem = function(ul, item) {
-		return $("<li></li>").data("item.autocomplete", item).append("<a>" + item.Name + "</a>").appendTo(ul);
-	};
+                if (addToList) {
+                    data.push({
+                        Name: term,
+                        Id: 0
+                    });
+                }
+                response(data);
+            });
+        },
+        select: function (event, ui) {
+            OnInterestAdded(ui.item);
+            this.value = "";
+            return false;
+        },
+        autoFocus: true //select the first element on the list
+    }).data("autocomplete")._renderItem = function (ul, item) {
+        return $("<li></li>").data("item.autocomplete", item).append("<a>" + item.Name + "</a>").appendTo(ul);
+    };
 
 	//whenever the link 'a.remove-interest' is clicked
 	//it should remove the interest from the list of added interests
