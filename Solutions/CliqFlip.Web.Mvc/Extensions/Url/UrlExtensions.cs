35abb552d2c1bafee350d0725adb714c594cbb53
Scott Coates <devnull@localhost>
2012-05-02 16:12:51 -0700

    added to public url

--- Solutions/CliqFlip.Web.Mvc/Extensions/Url/UrlExtensions.cs
+++ Solutions/CliqFlip.Web.Mvc/Extensions/Url/UrlExtensions.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Net.Mail;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using CliqFlip.Infrastructure.Common;
-using CliqFlip.Web.Mvc.Extensions.Request;
 
 namespace CliqFlip.Web.Mvc.Extensions.Url
 {
@@ -12,6 +10,27 @@ namespace CliqFlip.Web.Mvc.Extensions.Url
 		public static string ContentWithVersioning(this UrlHelper helper, string fileName)
 		{
 			return helper.Content(AssemblyHelper.GeneratePath(fileName));
+		}
+
+		public static string ToPublicUrl(this UrlHelper urlHelper, Uri relativeUri)
+		{
+			//http://support.appharbor.com/kb/getting-started/workaround-for-generating-absolute-urls-without-port-number
+			HttpContextBase httpContext = urlHelper.RequestContext.HttpContext;
+
+			var uriBuilder = new UriBuilder
+			{
+				Host = httpContext.Request.Url.Host,
+				Path = "/",
+				Port = 80,
+				Scheme = "http",
+			};
+
+			if (httpContext.Request.IsLocal)
+			{
+				uriBuilder.Port = httpContext.Request.Url.Port;
+			}
+
+			return new Uri(uriBuilder.Uri, relativeUri).AbsoluteUri;
 		}
 	}
 }
