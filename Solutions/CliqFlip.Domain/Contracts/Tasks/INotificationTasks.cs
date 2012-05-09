6370ad192ac1b9cc0d9f05724b0bb4bb9b24c53b
Scott Coates <devnull@localhost>
2012-05-09 12:38:16 -0700

    added notification tasks

---
+++ Solutions/CliqFlip.Domain/Contracts/Tasks/INotificationTasks.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using CliqFlip.Domain.Dtos;
+using CliqFlip.Domain.Entities;
+using CliqFlip.Domain.ValueObjects;
+
+namespace CliqFlip.Domain.Contracts.Tasks
+{
+	public interface  INotificationTasks
+	{
+		Notification GetMostRecentNotification();
+	}
+}
---
+++ Solutions/CliqFlip.Tasks/TaskImpl/NotificationTasks.cs
@@ -0,0 +1,13 @@
+using CliqFlip.Domain.Contracts.Tasks;
+using CliqFlip.Domain.Entities;
+
+namespace CliqFlip.Tasks.TaskImpl
+{
+	public class NotificationTasks : INotificationTasks
+	{
+		public Notification GetMostRecentNotification()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
