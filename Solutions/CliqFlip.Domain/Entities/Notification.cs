b25456f1caca190949ce5dae5134444e8b67519d
Scott Coates <devnull@localhost>
2012-05-09 00:24:05 -0700

    added notifications

---
+++ Solutions/CliqFlip.Domain/Entities/Notification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SharpArch.Domain.DomainModel;
+
+namespace CliqFlip.Domain.Entities
+{
+	public class Notification : Entity
+	{
+		public virtual string Message { get; set; }
+		public virtual DateTime CreateDate { get; set; }
+	}
+}
