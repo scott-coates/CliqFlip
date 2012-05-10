9c7f2a57ab900089f6d65e4e6cb9e8660f937fbf
Scott Coates <devnull@localhost>
2012-05-09 23:25:48 -0700

    modified notification

---
+++ Solutions/CliqFlip.Web.Mvc/Content/js/notification/notification.js
@@ -0,0 +1,22 @@
+﻿function SetupNotifications(parameters) {
+	function persist() {
+		$.cookie("@Model.NotificationCookie", "@Model.Id", { expires: 365, path: "/" });
+		setTimeout(showBody, 500);
+	}
+
+	function showBody() {
+		$('body').animate({
+			'padding-top': '0'
+		}, 500);
+	}
+
+	var notification = null;
+	$('body').animate({
+		'padding-top': '50px'
+	}, 1000, function () {
+		notification = $('#notification').notify({ type: 'sticky', onClose: persist });
+
+		//make every link popout
+		$("a:not(.close)", notification).attr('target', '_blank');
+	});
+}
