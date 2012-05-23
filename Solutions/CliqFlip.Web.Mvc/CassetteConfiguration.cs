bba565ff74986c3d1801d2cb656f8abcea5b6008
Scott Coates <devnull@localhost>
2012-05-23 00:09:27 -0700

    added template

--- Solutions/CliqFlip.Web.Mvc/CassetteConfiguration.cs
+++ Solutions/CliqFlip.Web.Mvc/CassetteConfiguration.cs
@@ -1,41 +1,42 @@
 using Cassette.Configuration;
+using Cassette.HtmlTemplates;
 using Cassette.Scripts;
 using Cassette.Stylesheets;
 
 namespace CliqFlip.Web.Mvc
 {
-    /// <summary>
-    /// Configures the Cassette asset modules for the web application.
-    /// </summary>
-    public class CassetteConfiguration : ICassetteConfiguration
-    {
-        public void Configure(BundleCollection bundles, CassetteSettings settings)
-        {
-            /* TODO: 
+	/// <summary>
+	/// Configures the Cassette asset modules for the web application.
+	/// </summary>
+	public class CassetteConfiguration : ICassetteConfiguration
+	{
+		public void Configure(BundleCollection bundles, CassetteSettings settings)
+		{
+			/* TODO: 
 			 * remove all min js files
 			 * remove duplicate code for the tag cloude
 			 * remove script ref's in our views
 			 * place comment dependencies in our scripts
 			 */
-            // Please read http://getcassette.net/documentation/configuration
+			// Please read http://getcassette.net/documentation/configuration
 
-            // This default configuration treats each file as a separate 'bundle'.
-            // In production the content will be minified, but the files are not combined.
-            // So you probably want to tweak these defaults!
-            bundles.AddPerIndividualFile<StylesheetBundle>("Content");
-            bundles.AddPerIndividualFile<ScriptBundle>("Scripts");
+			// This default configuration treats each file as a separate 'bundle'.
+			// In production the content will be minified, but the files are not combined.
+			// So you probably want to tweak these defaults!
+			bundles.AddPerIndividualFile<StylesheetBundle>("Content");
+			bundles.AddPerIndividualFile<ScriptBundle>("Scripts");
+			bundles.Add<HtmlTemplateBundle>("Content/templates", bundle => bundle.Processor = new HoganPipeline());
+			// To combine files, try something like this instead:
+			//   bundles.Add<StylesheetBundle>("Content");
+			// In production mode, all of ~/Content will be combined into a single bundle.
 
-            // To combine files, try something like this instead:
-            //   bundles.Add<StylesheetBundle>("Content");
-            // In production mode, all of ~/Content will be combined into a single bundle.
-            
-            // If you want a bundle per folder, try this:
-            //   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
-            // Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
-            // This is useful when there are lots of scripts for different areas of the website.
+			// If you want a bundle per folder, try this:
+			//   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
+			// Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
+			// This is useful when there are lots of scripts for different areas of the website.
 
-            // *** TOP TIP: Delete all ".min.js" files now ***
-            // Cassette minifies scripts for you. So those files are never used.
-        }
-    }
+			// *** TOP TIP: Delete all ".min.js" files now ***
+			// Cassette minifies scripts for you. So those files are never used.
+		}
+	}
 }
