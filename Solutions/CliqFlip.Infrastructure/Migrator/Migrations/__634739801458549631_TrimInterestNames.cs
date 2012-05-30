5f551dbe53fe0b72acf3c675f372428d4b4c195c
Scott Coates <devnull@localhost>
2012-05-30 13:17:42 -0700

    trim data

---
+++ Solutions/CliqFlip.Infrastructure/Migrator/Migrations/__634739801458549631_TrimInterestNames.cs
@@ -0,0 +1,19 @@
+﻿using Migrator.Framework;
+using MigratorNeo4j.CliqFlip;
+using Neo4jClient;
+
+namespace CliqFlip.Infrastructure.Migrator.Migrations
+{
+	[Migration(634739801458549631)]
+	public class __634739801458549631_TrimInterestNames : Migration
+	{
+		public override void Up()
+		{
+			Database.ExecuteNonQuery("UPDATE Interests SET Name = LTRIM(RTRIM(Name))");
+		}
+
+		public override void Down()
+		{
+		}
+	}
+}
