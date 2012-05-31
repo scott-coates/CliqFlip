0779f1241b5f027fb7db3e3a82d678ff757b1900
Scott Coates <devnull@localhost>
2012-05-31 12:12:49 -0700

    fix slug migration

---
+++ Solutions/CliqFlip.Infrastructure/Migrator/Migrations/__634740630612747240_FixSlugs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using Migrator.Framework;
+using MigratorNeo4j.CliqFlip;
+using Neo4jClient;
+
+namespace CliqFlip.Infrastructure.Migrator.Migrations
+{
+	[Migration(634740630612747240)]
+	public class __634740630612747240_FixSlugs : Migration
+	{
+		public override void Up()
+		{
+			//some of the very first interests were not slugged
+			Database.ExecuteNonQuery("UPDATE Interests SET Slug = REPLACE(LOWER(Slug), ' ', '-') WHERE ID <= 10");
+		}
+
+		public override void Down()
+		{
+		}
+	}
+}
