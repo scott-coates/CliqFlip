21e264890a9e77a5991833a2eba3dedf08d9f0fd
Scott Coates <devnull@localhost>
2012-06-11 21:22:46 -0700

    added test for perfect hop

--- Solutions/CliqFlip.Tests/Unit/Pipelines/UserSearch/UserTaskTests.cs
+++ Solutions/CliqFlip.Tests/Unit/Pipelines/UserSearch/UserTaskTests.cs
@@ -1,28 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CliqFlip.Domain.Common;
 using CliqFlip.Domain.Contracts.Pipelines.UserSearch;
 using CliqFlip.Domain.Contracts.Pipelines.UserSearch.Filters;
+using CliqFlip.Domain.Dtos.Interest;
 using CliqFlip.Tasks.Pipelines.UserSearch.Filters;
 using NUnit.Framework;
 
 namespace CliqFlip.Tests.Unit.Pipelines.UserSearch
 {
-	[TestFixture]
+    [TestFixture]
     public class ScoreRelatedInterestFilterTests
-	{
-	    private readonly IScoreRelatedInterestFilter _scoreRelatedInterestFilter = new ScoreRelatedInterestFilter();
+    {
+        private readonly IScoreRelatedInterestFilter _scoreRelatedInterestFilter = new ScoreRelatedInterestFilter();
+        private static readonly int _maxHopsInverter = int.Parse(Constants.INTEREST_MAX_HOPS) + 1;
 
-		[SetUp]
-		public void Setup()
-		{
-		    
-		}
+        [SetUp]
+        public void Setup()
+        {
+        }
 
-		
         [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
-		public void NullInputReturnsError()
-		{
-		    _scoreRelatedInterestFilter.Filter(null);		
+        [ExpectedException(typeof (ArgumentNullException))]
+        public void NullInputReturnsError()
+        {
+            _scoreRelatedInterestFilter.Filter(null);
+        }
+
+        [Test]
+        public void ExactMatchIsEqualToMaxHopsCounter()
+        {
+            var userSearchPipelineResult = new UserSearchPipelineResult
+            {
+                RelatedInterests = new List<WeightedRelatedInterestDto>
+                {
+                    new WeightedRelatedInterestDto(0, new List<float>(), "")
+                }
+            };
+
+            _scoreRelatedInterestFilter.Filter(userSearchPipelineResult);
+
+            Assert.That(userSearchPipelineResult.ScoredInterests.Single().Score, Is.EqualTo(_maxHopsInverter));
         }
 
         //[TestCase("http://somewebsite.com/")]
@@ -34,5 +53,5 @@ namespace CliqFlip.Tests.Unit.Pipelines.UserSearch
         //    _userTasks.SaveWebsite(_user, url);
         //    Assert.Pass("Website Url");
         //}
-	}
+    }
 }
