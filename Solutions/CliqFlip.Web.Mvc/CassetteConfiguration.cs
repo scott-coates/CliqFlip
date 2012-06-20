4736f2bc828203fa17a4705b2b2526497ab86abd
Scott Coates <devnull@localhost>
2012-06-20 00:15:31 -0700

    [mq]: cassette

--- Solutions/CliqFlip.Web.Mvc/CassetteConfiguration.cs
+++ Solutions/CliqFlip.Web.Mvc/CassetteConfiguration.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Cassette.Configuration;
 using Cassette.HtmlTemplates;
 using Cassette.Scripts;
@@ -5,38 +6,32 @@ using Cassette.Stylesheets;
 
 namespace CliqFlip.Web.Mvc
 {
-	/// <summary>
-	/// Configures the Cassette asset modules for the web application.
-	/// </summary>
-	public class CassetteConfiguration : ICassetteConfiguration
-	{
-		public void Configure(BundleCollection bundles, CassetteSettings settings)
-		{
-			/* TODO: 
-			 * remove all min js files
-			 * remove duplicate code for the tag cloude
-			 * remove script ref's in our views
-			 * place comment dependencies in our scripts
-			 */
-			// Please read http://getcassette.net/documentation/configuration
+    /// <summary>
+    /// Configures the Cassette asset modules for the web application.
+    /// </summary>
+    public class CassetteConfiguration : ICassetteConfiguration
+    {
+        public void Configure(BundleCollection bundles, CassetteSettings settings)
+        {
+            //css, less, sass
+            bundles.AddPerSubDirectory<StylesheetBundle>("Content/assets/styles");
 
-			// This default configuration treats each file as a separate 'bundle'.
-			// In production the content will be minified, but the files are not combined.
-			// So you probably want to tweak these defaults!
-			bundles.AddPerIndividualFile<StylesheetBundle>("Content");
-			bundles.AddPerIndividualFile<ScriptBundle>("Scripts");
-			bundles.Add<HtmlTemplateBundle>("Content/assets/templates", bundle => bundle.Processor = new HoganPipeline());
-			// To combine files, try something like this instead:
-			//   bundles.Add<StylesheetBundle>("Content");
-			// In production mode, all of ~/Content will be combined into a single bundle.
+            //this dir is for plugins, site js, etc...ignore cliqflip stuff tho
+            bundles.AddPerSubDirectory<ScriptBundle>("Content/assets/js", new FileSearch { Exclude = new Regex("js\\cliqflip") });
 
-			// If you want a bundle per folder, try this:
-			//   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
-			// Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
-			// This is useful when there are lots of scripts for different areas of the website.
+            //put all of cliqflip in one file
+            bundles.Add<ScriptBundle>("Content/assets/js/cliqflip", new FileSearch { Exclude = new Regex("js\\cliqflip\\error-handler.js") });
+            bundles.Add<ScriptBundle>("Content/assets/js/cliqflip/error-handler.js", bundle => bundle.PageLocation = "head");
 
-			// *** TOP TIP: Delete all ".min.js" files now ***
-			// Cassette minifies scripts for you. So those files are never used.
-		}
-	}
+            //NuGet
+            bundles.Add<ScriptBundle>("Scripts", new FileSearch { Exclude = new Regex("modernizr") });
+
+            //only modernizr
+            //http://stackoverflow.com/questions/1153856/string-negation-using-regular-expressions
+            bundles.Add<ScriptBundle>("Scripts", new FileSearch { Exclude = new Regex("^(?!.*modernizr).*$") }, bundle => bundle.PageLocation = "head");
+
+            //templates
+            bundles.Add<HtmlTemplateBundle>("Content/assets/templates", bundle => bundle.Processor = new HoganPipeline());
+        }
+    }
 }
--- Solutions/CliqFlip.Web.Mvc/Content/assets/js/cliqFlip/validation/jquery.validate.must-be-true.js
+++
@@ -1,5 +0,0 @@
-﻿jQuery.validator.addMethod("must_be_true", function (value, element, options) {
-	return element.checked;
-});
-
-$.validator.unobtrusive.adapters.addBool("mustbetrue", "must_be_true"); 
