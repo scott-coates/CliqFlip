0c7d51de9d13247e96b4256fe6087514ead01da3
Scott Coates <devnull@localhost>
2012-07-02 16:48:33 -0700

    comment

--- Solutions/CliqFlip.Infrastructure/NHibernate/Maps/Overrides/WebPageMappingOverride.cs
+++ Solutions/CliqFlip.Infrastructure/NHibernate/Maps/Overrides/WebPageMappingOverride.cs
@@ -4,11 +4,11 @@ using FluentNHibernate.Automapping.Alterations;
 
 namespace CliqFlip.Infrastructure.NHibernate.Maps.Overrides
 {
-	public class WebPageMappingOverride : IAutoMappingOverride<WebPage>
-	{
-		public void Override(AutoMapping<WebPage> mapping)
-		{
-			mapping.Table("WebPages");
-		}
-	}
+    public class WebPageMappingOverride : IAutoMappingOverride<WebPage>
+    {
+        public void Override(AutoMapping<WebPage> mapping)
+        {
+            mapping.Table("WebPages"); //required because it's a subclass
+        }
+    }
 }
