15dc67183901283a0a43f407e2df0c88d9862cbd
Scott Coates <devnull@localhost>
2012-07-07 05:20:58 -0700

    infinite scroll

--- Solutions/CliqFlip.Web.Mvc/Content/app/collections/feed/feedList.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/collections/feed/feedList.js
@@ -3,10 +3,25 @@
 
 var CliqFlip = (function (cliqFlip) {
 
+    function incrementPage() {
+        this.page = this.page || 1;
+        this.page++;
+    }
+
     cliqFlip.Mvc.App.Collections.FeedList = Backbone.Collection.extend(
         {
             model: cliqFlip.Mvc.App.Models.FeedItem,
-            url: '/search/interestfeed'
+            url: '/search/interestfeed',
+            page: null,
+            fetch: function(options) {
+                options = options || {};
+                if(this.page) {
+                    options.data = options.data || {};
+                    options.data.page = this.page;
+                }
+                incrementPage.call(this);
+                return Backbone.Collection.prototype.fetch.call(this, options);
+            }
         });
 
     return cliqFlip;
--- Solutions/CliqFlip.Web.Mvc/Content/app/views/feed/feedListView.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/views/feed/feedListView.js
@@ -5,7 +5,31 @@ var CliqFlip = (function(cliqFlip) {
     cliqFlip.Mvc.App.Views.FeedListView = Backbone.Marionette.CompositeView.extend({
         template: "feed-feedList",
         className: 'feed-list invisible',
+        triggerPoint: 400,
         itemView: cliqFlip.Mvc.App.Views.FeedItemView,
+        initialize: function() {
+            // isLoading is a useful flag to make sure we don't send off more than
+            // one request at a time
+            //            http: //stackoverflow.com/questions/9110060/how-do-i-add-a-resize-event-to-the-window-in-a-view-using-backbone
+            this.isLoading = false;
+            $(window).bind("scroll", null, _.bind(this.checkScroll, this));
+        },
+        checkScroll: function() {
+            if(!this.isLoading && ($(window).scrollTop() >= $(document).height() - $(window).height() - this.triggerPoint)) {
+                this.loadResults();
+            }
+        },
+        loadResults: function() {
+            var that = this;
+            this.isLoading = true;
+            this.collection.fetch({
+                add: true,
+                success: function(models) {
+                    that.isLoading = false;
+                    that.$el.masonry('reload');
+                }
+            });
+        },
         onShow: function() {
             var that = this;
             this.$el.imagesLoaded(function() {
