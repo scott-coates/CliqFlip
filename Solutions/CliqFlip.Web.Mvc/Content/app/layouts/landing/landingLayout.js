7f127e56ab419384a047949d6633e19e383faa91
Scott Coates <devnull@localhost>
2012-07-06 18:10:12 -0700

    container fluid

--- Solutions/CliqFlip.Web.Mvc/Content/app/layouts/landing/landingLayout.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/layouts/landing/landingLayout.js
@@ -2,7 +2,7 @@ var CliqFlip = (function(cliqFlip) {
 
     cliqFlip.Mvc.App.Layouts.LandingLayout = Backbone.Marionette.Layout.extend({
         template: "landing-layout",
-        className: "container gray-rounded-border",
+        className: "container-fluid gray-rounded-border",
         regions: {
             leftColumnRegion: "#left-column",
             contentAreaRegion: "#content-area"
