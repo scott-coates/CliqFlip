d5c3ca1ed28f555ad59e8d13486fbe0179b27e59
Scott Coates <devnull@localhost>
2012-07-13 12:50:55 -0700

    added click

--- Solutions/CliqFlip.Web.Mvc/Content/app/views/feed/feedPostOverviewView.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/views/feed/feedPostOverviewView.js
@@ -2,8 +2,14 @@ var CliqFlip = (function(cliqFlip) {
 
     cliqFlip.App.Mvc.Views.FeedPostOverviewView = Backbone.Marionette.ItemView.extend({
         template: "feed-feedPostOverview",
-        className:"post-overview full-height"
+        className: "post-overview full-height",
+        events: {
+            "click .post-overview-comment-button": "addComment"
+        },
+        addComment: function(parameters) {
+            alert(parameters);
+        }
     });
 
     return cliqFlip;
-}(CliqFlip));
+} (CliqFlip));
