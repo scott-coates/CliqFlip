24f253dd924ff4fa5c00b939b790aaa7bdfadca4
Scott Coates <devnull@localhost>
2012-07-14 11:52:55 -0700

    changed styling

--- Solutions/CliqFlip.Web.Mvc/Queries/FeedPostOverviewQuery.cs
+++ Solutions/CliqFlip.Web.Mvc/Queries/FeedPostOverviewQuery.cs
@@ -24,7 +24,7 @@ namespace CliqFlip.Web.Mvc.Queries
             var post = _postTasks.Get(postId);
             retVal.Username = post.UserInterest.User.Username;
             retVal.Headline = post.UserInterest.User.Headline;
-            retVal.AuthorImageUrl = post.UserInterest.User.ProfileImage.ImageData.FullFileName;
+            retVal.AuthorImageUrl = post.UserInterest.User.ProfileImage.ImageData.MediumFileName;
             retVal.ImageDescription = post.Description;
             var image = post.Medium as Image;
             if(image != null)
