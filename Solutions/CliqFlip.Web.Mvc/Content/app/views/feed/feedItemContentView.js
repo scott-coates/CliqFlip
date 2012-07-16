bb6e8033bae430d41d6df36b97b901d990c53c01
Jorge <Jorge@Jorge-PC>
2012-07-16 00:52:16 -0700

    removed unneeded view

--- Solutions/CliqFlip.Web.Mvc/Content/app/views/feed/feedItemContentView.js
+++
@@ -1,8 +0,0 @@
-var CliqFlip = (function(cliqFlip) {
-
-    cliqFlip.App.Mvc.Views.FeedItemContentView = Backbone.Marionette.ItemView.extend({
-        templateHelpers: cliqFlip.ViewHelpers,
-    });
-
-    return cliqFlip;
-} (CliqFlip));
