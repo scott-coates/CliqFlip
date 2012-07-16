8a3223952748a558b2d7b0e45aac9b00bea7fba9
Scott Coates <devnull@localhost>
2012-07-16 02:38:13 -0700

    auto unbind

--- Solutions/CliqFlip.Web.Mvc/Content/app/layouts/landing/feedPostOverviewLayout.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/layouts/landing/feedPostOverviewLayout.js
@@ -2,7 +2,7 @@ var CliqFlip = (function(cliqFlip) {
 
     cliqFlip.App.Mvc.Layouts.FeedPostOverviewLayout = Backbone.Marionette.Layout.extend({
         initialize: function() {
-            cliqFlip.App.Mvc.vent.bind("comment:posted:success", this.clearComment);
+            this.bindTo(cliqFlip.App.Mvc.vent, "comment:posted:success", this.clearComment); //bindTo will automatically be unbound on Close
         },
         template: "feed-feedPostOverview",
         className: "post-overview full-height",
@@ -20,9 +20,6 @@ var CliqFlip = (function(cliqFlip) {
         clearComment: function() {
             var commentElem = $("#post-overview-comment-content", this.$el);
             commentElem.val('');
-        },
-        onClose: function() {
-            cliqFlip.App.Mvc.vent.unbind("comment:posted:success", this.clearComment);
         }
     });
 
