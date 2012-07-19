250406ae6510001905d7a646882b1c0acee42dfa
Scott Coates <devnull@localhost>
2012-07-19 12:28:22 -0700

    removed duplicate client validaotr

--- Solutions/CliqFlip.Web.Mvc/Global.asax.cs
+++ Solutions/CliqFlip.Web.Mvc/Global.asax.cs
@@ -111,8 +111,6 @@ namespace CliqFlip.Web.Mvc
 
 			ModelBinders.Binders.DefaultBinder = new SharpModelBinder();
 
-			ModelValidatorProviders.Providers.Add(new ClientDataTypeModelValidatorProvider());
-
 			InitializeServiceLocator();
 
 			AreaRegistration.RegisterAllAreas();
