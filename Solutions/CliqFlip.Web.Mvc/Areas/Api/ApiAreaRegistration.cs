d5418a353ff1ecfccc80d976830580202391a843
Scott Coates <devnull@localhost>
2012-07-21 02:44:00 -0700

    added default route

--- Solutions/CliqFlip.Web.Mvc/Areas/Api/ApiAreaRegistration.cs
+++ Solutions/CliqFlip.Web.Mvc/Areas/Api/ApiAreaRegistration.cs
@@ -16,10 +16,14 @@ namespace CliqFlip.Web.Mvc.Areas.Api
         public override void RegisterArea(AreaRegistrationContext context)
         {
             context.Routes.MapHttpRoute(
-                "Api_default",
-                "Api/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional } ,
-				new[] { "CliqFlip.Web.Mvc.Areas.Api.Controllers" });
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+            //context.Routes.MapHttpRoute(
+            //    "Api_default",
+            //    "Api/{controller}/{action}/{id}",
+            //    new { action = "Index", id = UrlParameter.Optional } ,
+            //    new[] { "CliqFlip.Web.Mvc.Areas.Api.Controllers" });
         }
     }
 }
