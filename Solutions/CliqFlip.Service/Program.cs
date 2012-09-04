ac84233a2c6255fc9a2ddf3f5c42a343d5f5bca0
Scott Coates <devnull@localhost>
2012-09-03 19:22:20 -0700

    added cliqflip service

---
+++ Solutions/CliqFlip.Service/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Topshelf;
+
+namespace CliqFlip.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<Service>();
+                x.RunAsLocalSystem();
+                x.SetServiceName("CliqFlip.Service");
+            });
+        }
+    }
+}
---
+++ Solutions/CliqFlip.Service/Service.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Topshelf;
+using Topshelf.Logging;
+
+namespace CliqFlip.Service
+{
+    internal class Service : ServiceControl
+    {
+        private static readonly LogWriter _log = HostLogger.Get<Service>();
+
+        public bool Start(HostControl hostControl)
+        {
+            _log.Info("SampleService Starting...");
+
+            hostControl.RequestAdditionalTime(TimeSpan.FromSeconds(10));
+
+            Thread.Sleep(1000);
+
+            ThreadPool.QueueUserWorkItem(
+                x =>
+                {
+                    Thread.Sleep(3000);
+
+                    _log.Info("Requesting stop");
+
+                    hostControl.Stop();
+                });
+            _log.Info("SampleService Started");
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _log.Info("SampleService Stopped");
+
+            return true;
+        }
+
+        public bool Pause(HostControl hostControl)
+        {
+            _log.Info("SampleService Paused");
+
+            return true;
+        }
+
+        public bool Continue(HostControl hostControl)
+        {
+            _log.Info("SampleService Continued");
+
+            return true;
+        }
+    }
+}
