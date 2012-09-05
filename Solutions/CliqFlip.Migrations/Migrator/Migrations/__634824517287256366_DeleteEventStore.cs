277323f51f7e84716798a2c75d948ec66e96127a
Scott Coates <devnull@localhost>
2012-09-05 14:26:26 -0700

    updated commits

---
+++ Solutions/CliqFlip.Migrations/Migrator/Migrations/__634824517287256366_DeleteEventStore.cs
@@ -0,0 +1,29 @@
+﻿#region
+
+using Migrator.Framework;
+
+#endregion
+
+namespace CliqFlip.Migrations.Migrator.Migrations
+{
+    [Migration(634824517287256366)]
+    public class __634824517287256366_DeleteEventStore : Migration
+    {
+        public override void Up()
+        {
+            
+        }
+
+        public override void Down()
+        {
+            if(Database.TableExists("Commits"))
+            {
+                Database.RemoveTable("Commits");
+            }
+            if (Database.TableExists("Snapshots"))
+            {
+                Database.RemoveTable("Snapshots");
+            }
+        }
+    }
+}
