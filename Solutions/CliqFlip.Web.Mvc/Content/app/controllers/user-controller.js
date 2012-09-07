f0d0b4a6314acd68ca9fc324e50d2dbf435f38eb
Scott Coates <devnull@localhost>
2012-09-07 16:26:48 -0700

    updated ng

--- Solutions/CliqFlip.Web.Mvc/Content/app/controllers/user-controller.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/controllers/user-controller.js
@@ -3,4 +3,15 @@
 /* Controllers */
 
 function UserController($scope, $http) {
+    $scope.users = [
+    { "name": "Nexus S",
+        "snippet": "Fast just got faster with Nexus S."
+    },
+    { "name": "Motorola XOOM! with Wi-Fi",
+        "snippet": "The Next, Next Generation tablet."
+    },
+    { "name": "MOTOROLA XOOM!",
+        "snippet": "The Next, Next Generation tablet."
+    }
+  ];
 }
