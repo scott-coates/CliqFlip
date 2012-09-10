4e54589d5a4de674003c77c885686368f70117a5
Scott Coates <devnull@localhost>
2012-09-09 23:38:32 -0700

    added angular app

---
+++ Solutions/CliqFlip.Web.Mvc/Content/app/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('cliqFlipApp', []).
+  config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/', { templateUrl: '/Content/app/partials/suggested-user-list.html', controller: UserController });
+  }]);
