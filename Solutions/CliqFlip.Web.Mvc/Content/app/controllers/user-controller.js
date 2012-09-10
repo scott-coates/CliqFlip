3534f477a3ba7c8cbdf33ef625bf881be7d357bd
Scott Coates <devnull@localhost>
2012-09-10 12:59:40 -0700

    output data=

--- Solutions/CliqFlip.Web.Mvc/Content/app/controllers/user-controller.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/controllers/user-controller.js
@@ -4,7 +4,7 @@
 
 function UserController($scope, $resource) {
     $scope.SuggestedUser = $resource('/api/suggesteduser/'); /*TODO Look into naming this suggested-user*/
-    $scope.users = $scope.SuggestedUser.get(function (data) {
-        console.log(data);
+    $scope.users = $scope.SuggestedUser.get(function (data, data2, data3) {
+        debugger;
     });
 }
