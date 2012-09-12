5792f77fe4bbc79fea8fcbd74d96b038235b4837
Scott Coates <devnull@localhost>
2012-09-12 12:12:43 -0700

    suggested user column

--- Solutions/CliqFlip.Web.Mvc/Content/app/app.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/app.js
@@ -4,5 +4,5 @@
 // Declare app level module which depends on filters, and services
 angular.module('cliqFlipApp', ['ngResource', 'cliqFlipServices', 'cliqFlipDirectives']).
   config(['$routeProvider', function ($routeProvider) {
-      $routeProvider.when('/', { templateUrl: '/Content/app/partials/suggested-user-list.html', controller: UserController });
+      $routeProvider.when('/', { templateUrl: '/Content/app/partials/suggested-users.html', controller: UserController });
   } ]);
