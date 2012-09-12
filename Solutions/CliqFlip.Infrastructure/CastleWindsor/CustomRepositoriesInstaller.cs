f4e21a54f8de2353c0cc23d0e0b680912c674e73
Scott Coates <devnull@localhost>
2012-09-12 06:25:08 -0700

    change hostname

--- Solutions/CliqFlip.Infrastructure/CastleWindsor/CustomRepositoriesInstaller.cs
+++ Solutions/CliqFlip.Infrastructure/CastleWindsor/CustomRepositoriesInstaller.cs
@@ -57,7 +57,7 @@ namespace CliqFlip.Infrastructure.CastleWindsor
                         .LifeStyle.Singleton.UsingFactoryMethod(
                             () =>
                             {
-                                var client = new RedisClient("192.168.83.140", 6379);
+                                var client = new RedisClient("ubuntu", 6379);
                                 client.DbSize.ToString();//will cause exception right here right now
                                 return client;
                             }));
