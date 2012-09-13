f2d5f01188e19a31309931d7d9aac75007042d53
Scott Coates <devnull@localhost>
2012-09-13 15:44:21 -0700

    user page

--- Solutions/CliqFlip.Web.Mvc/Content/app/app.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/app.js
@@ -3,6 +3,7 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('cliqFlipApp', ['ngResource', 'cliqFlipServices', 'cliqFlipDirectives']).
-  config(['$routeProvider', function ($routeProvider) {
-      $routeProvider.when('/', { templateUrl: '/Content/app/partials/suggested-users.html', controller: UserController });
+  config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+      $locationProvider.html5Mode(true);
+      $routeProvider.when('/u', { templateUrl: '/Content/app/partials/suggested-users.html', controller: UserController });
   } ]);
