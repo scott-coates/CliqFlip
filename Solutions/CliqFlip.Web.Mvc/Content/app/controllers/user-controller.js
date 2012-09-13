5a695ea7daa8afd74d1c874ec49e75f254b0fab9
Scott Coates <devnull@localhost>
2012-09-12 17:50:35 -0700

    user data

--- Solutions/CliqFlip.Web.Mvc/Content/app/controllers/user-controller.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/controllers/user-controller.js
@@ -5,7 +5,11 @@
 function UserController($scope, $resource, UserData) {
     $scope.SuggestedUser = $resource('/api/suggesteduser/'); /*TODO Look into naming this suggested-user*/
     updateUsers();
-    
+
+    $scope.selectUser = function (user) {
+        $scope.selectedUser = user;
+    };
+
     var pusher = new window.Pusher('3aa270fd00dec97e5b04');
     var channel = pusher.subscribe('suggested-user-queue-' + UserData.Username.toString());
     channel.bind('update', function () {
