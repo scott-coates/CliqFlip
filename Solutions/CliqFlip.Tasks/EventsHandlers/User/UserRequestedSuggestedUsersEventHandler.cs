3d2aab9680f31c5d1a6b10a5ccd86a6754f37a18
Scott Coates <devnull@localhost>
2012-09-14 21:54:14 -0700

    moved responsibility

--- Solutions/CliqFlip.Tasks/EventsHandlers/User/UserRequestedSuggestedUsersEventHandler.cs
+++ Solutions/CliqFlip.Tasks/EventsHandlers/User/UserRequestedSuggestedUsersEventHandler.cs
@@ -15,14 +15,12 @@ namespace CliqFlip.Tasks.EventsHandlers.User
     {
         private readonly IUserSearchPipeline _userSearchPipeline;
         private readonly IUserTasks _userTasks;
-        private readonly IUserInterestTasks _userInterestTasks;
         private readonly IPusherProvider _pusherProvider;
 
-        public UserRequestedSuggestedUsersEventHandler(IUserSearchPipeline userSearchPipeline, IUserTasks userTasks, IUserInterestTasks userInterestTasks, IPusherProvider pusherProvider)
+        public UserRequestedSuggestedUsersEventHandler(IUserSearchPipeline userSearchPipeline, IUserTasks userTasks, IPusherProvider pusherProvider)
         {
             _userSearchPipeline = userSearchPipeline;
             _userTasks = userTasks;
-            _userInterestTasks = userInterestTasks;
             _pusherProvider = pusherProvider;
         }
 
@@ -34,23 +32,7 @@ namespace CliqFlip.Tasks.EventsHandlers.User
                 var user = _userTasks.GetUser(message.Username);
                 var pipelineRequest = new UserSearchPipelineRequest { User = user, LocationData = user.Location.Data };
                 var pipelineResult = _userSearchPipeline.Execute(pipelineRequest);
-                foreach (var userSearchResult in pipelineResult.Users)
-                {
-                    var commonInterest = _userInterestTasks.GetInterestsInCommon(user, _userTasks.GetUser(userSearchResult.Username));
-                    userSearchResult.DirectInterestCount = commonInterest.Count(x => x.IsExactMatch);
-                    userSearchResult.IndirectInterestCount = commonInterest.Count(x => !x.IsExactMatch);
-                    userSearchResult.CommonInterestCount = userSearchResult.DirectInterestCount + userSearchResult.IndirectInterestCount;
-                    userSearchResult.InterestsInCommon = commonInterest
-                        .OrderByDescending(x => x.Score)
-                        .Select(
-                            x => new UserSearchResultDto.InterestInCommonDto
-                            {
-                                Name = x.Name,
-                                IsExactMatch = x.IsExactMatch
-                            })
-                        .ToList();
-                }
-
+                
                 _userTasks.SaveSuggestedUsers(user, pipelineResult.Users);
 
                 _pusherProvider.Trigger(new SimplePusherRequest("suggested-user-queue-" + message.Username, "update", string.Format("{{\"usersCount\": \"{0}\"}}", pipelineResult.Users.Count)));
--- Solutions/CliqFlip.Tasks/Pipelines/UserSearch/Filters/TransformUserToScoredUserFilter.cs
+++ Solutions/CliqFlip.Tasks/Pipelines/UserSearch/Filters/TransformUserToScoredUserFilter.cs
@@ -3,17 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using CliqFlip.Domain.Contracts.Pipelines.UserSearch;
 using CliqFlip.Domain.Contracts.Pipelines.UserSearch.Filters;
+using CliqFlip.Domain.Contracts.Tasks.Entities;
 using CliqFlip.Domain.Dtos.User;
 
 namespace CliqFlip.Tasks.Pipelines.UserSearch.Filters
 {
     public class TransformUserToScoredUserFilter : ITransformUserToScoredUserFilter
     {
+        private readonly IUserInterestTasks _userInterestTasks;
+
+        public TransformUserToScoredUserFilter(IUserInterestTasks userInterestTasks)
+        {
+            _userInterestTasks = userInterestTasks;
+        }
+
         public void Filter(UserSearchPipelineResult userSearchPipelineResult, UserSearchPipelineRequest request)
         {
             if (userSearchPipelineResult == null) throw new ArgumentNullException("userSearchPipelineResult");
+            if (request == null) throw new ArgumentNullException("request");
+            if (request.User == null) throw new ArgumentNullException("request.user");
 
-            userSearchPipelineResult.Users = userSearchPipelineResult.UserQuery.Select(x => new UserSearchResultDto(x)).ToList();
+            userSearchPipelineResult.Users = userSearchPipelineResult
+                .UserQuery
+                .ToList()
+                .Select(x =>
+                {
+                    var retVal = new UserSearchResultDto(x);
+                    var commonInterest = _userInterestTasks.GetInterestsInCommon(request.User, x);
+                    retVal.DirectInterestCount = commonInterest.Count(y => y.IsExactMatch);
+                    retVal.IndirectInterestCount = commonInterest.Count(y => !y.IsExactMatch);
+                    retVal.CommonInterestCount = retVal.DirectInterestCount + retVal.IndirectInterestCount;
+                    retVal.InterestsInCommon = commonInterest
+                        .OrderByDescending(y => y.Score)
+                        .Select(
+                            y => new UserSearchResultDto.InterestInCommonDto
+                            {
+                                Name = y.Name,
+                                IsExactMatch = y.IsExactMatch
+                            })
+                        .ToList();
+                    return retVal;
+                }).ToList();
         }
     }
 }
