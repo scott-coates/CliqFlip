c92dee898b46e967d5fc8e7aa6a409ab233d6fe5
Scott Coates <devnull@localhost>
2012-09-16 22:38:56 -0700

    persist user data

--- Solutions/CliqFlip.Web.Mvc/Content/app/controllers/user-controller.js
+++ Solutions/CliqFlip.Web.Mvc/Content/app/controllers/user-controller.js
@@ -5,9 +5,10 @@
 function UserController($scope, $resource, UserData) {
     var pusher = new window.Pusher(UserData.PusherAppKey);
     var channel = pusher.subscribe('suggested-user-queue-' + UserData.Username.toString());
+    $scope.users = [];
 
     channel.bind('pusher:subscription_succeeded', function (arg) {
-        updateUsers();        
+        updateUsers();
     });
 
     channel.bind('update', function (arg) {
@@ -27,10 +28,17 @@ function UserController($scope, $resource, UserData) {
     };
 
     function updateUsers() {
-        $scope.users = $scope.SuggestedUser.query(function (data) {
-            console.log("UPDATE USERS : " + data);
-        });
+        if (typeof UserData.suggestedUsers !== "undefined") {
+            $scope.users = UserData.suggestedUsers;
+            $scope.$apply();
+        }
+        else {
+            $scope.SuggestedUser.query(function (data) {
+                $scope.users = UserData.suggestedUsers = data;
+            });
+        }
         $(window).resize(); //TODO move this
     }
 }
+
 UserController.$inject = ['$scope', '$resource', 'UserData'];
